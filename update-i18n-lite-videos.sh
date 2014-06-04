e24e0662e8cba60b2ca7bbdcb8d80636b83679cf
James Irwin <james@khanacademy.org>
2014-06-04 10:03:44 -0700

    Move the step to update the list of lite videos to its own build.
    Summary:
    This step takes about 12-13 minutes sometimes more of the
    update-translations step.  Since it is really separate from the
    other steps of that build, and since we may want to end up
    running the other steps more frequently, I am moving this out to
    its own file.
    
    After pushing I'll create its own jenkins task, and we can have
    it run just once or twice a week.
    
    Test Plan:
    comment out the commit and run:
    tools/build-i18n-lite-videos.sh
    See that intl/translations is updated with new video_*.json files.
    
    Reviewers: csilvers
    
    Reviewed By: csilvers
    
    Differential Revision: http://phabricator.khanacademy.org/D9658

---
+++ update-i18n-lite-videos.sh
@@ -0,0 +1,26 @@
+WORKSPACE_ROOT=`pwd -P`
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+
+source "${SCRIPT_DIR}/build.lib"
+
+ensure_virtualenv
+( cd "$WEBSITE_ROOT" && "$MAKE" install_deps )
+
+decrypt_secrets_py_and_add_to_pythonpath
+
+cd "$WEBSITE_ROOT"
+
+# --- The actual work:
+
+# This lets us commit messages without a test plan
+export FORCE_COMMIT=1
+
+echo "Updating the list of videos we have for 'lite' languages."
+tools/import_translations.py intl/translations
+
+echo "Checking in new video-lists."
+safe_commit_and_push intl/translations \
+   -m "Automatic update of video_*.json" \
+   -m "(at webapp commit `git rev-parse HEAD`)"
+
+
