f561bcd6373ca91b0b0b6d66ae7f471128b01ee4
Craig Silverstein <csilvers@khanacademy.org>
2014-07-02 17:25:27 -0700

    Rewrite for use with the 'groovy postbuild' plugin.
    Auditors: chris
    
    Test plan:
    Fingers crossed.  Once this is on jenkins we can test it there.

--- cancel_downstream.groovy
+++ cancel_downstream.groovy
@@ -2,33 +2,34 @@
 // The build is specified via commandline args: job name and build
 // number.  For instance: "$0 make-check 1234"
 //
-// This is meant to be run as a post-build step when the build is
-// canceled.  (It doesn't hurt to run it when the build fails, or even
-// when it succeeds, though it will be a noop in those cases.)  This
-// lets you automatically cancel downstream jobs when you yourself are
-// canceled.  As long as the downstream jobs themselves are set up to
-// run this as a post-build step, this will recursively cancel *all*
-// downstream jobs.
+// This is meant to be run via the "groovy postbuild" plugin when the
+// build is canceled.  (It doesn't hurt to run it when the build
+// fails, or even when it succeeds, though it will be a noop in those
+// cases.)  This lets you automatically cancel downstream jobs when
+// you yourself are canceled.  As long as the downstream jobs
+// themselves are set up to run this as a post-build step, this will
+// recursively cancel *all* downstream jobs.
+//
+// The "groovy postbuild" plugin gives access to 'hudson' and to
+// 'manager.build', which is the current build.
 //
 // This returns the number of tasks canceled -- either from the queue
 // for while running.
 
 
-import jenkins.model.*;
-
-def upstreamProject = args[0];
-def upstreamBuildNumber = args[1];
+def upstreamProject = manager.build.project.name;
+def upstreamBuildNumber = manager.build.number;
 
 def numCancels = 0;
 
 // First, cancel builds waiting in the queue, so they never start.
-for (build in jenkins.model.Jenkins.instance.queue.items) {
+for (build in hudson.queue.items) {
     for (cause in build.causes) {
         if (!cause.hasProperty('upstreamProject')) { continue; }
         if (cause.upstreamProject == upstreamProject &&
-                cause.upstreamBuild.toString() == upstreamBuildNumber) {
+                cause.upstreamBuild == upstreamBuildNumber) {
             println 'Cancelling ' + build.toString();
-            jenkins.model.Jenkins.instance.queue.cancel(build.task);
+            hudson.queue.cancel(build.task);
             numCancels++;
             break;
         }
@@ -36,14 +37,14 @@ for (build in jenkins.model.Jenkins.instance.queue.items) {
 }
 
 // Now, cancel running builds.
-for (job in jenkins.model.Jenkins.instance.items) {
+for (job in hudson.instance.items) {
     for (build in job.builds) {
         if (!build.hasProperty('causes')) { continue; }
         if (!build.isBuilding()) { continue; }
         for (cause in build.causes) {
             if (!cause.hasProperty('upstreamProject')) { continue; }
             if (cause.upstreamProject == upstreamProject &&
-                    cause.upstreamBuild.toString() == upstreamBuildNumber) {
+                    cause.upstreamBuild == upstreamBuildNumber) {
                 println 'Stopping ' + build.toString();
                 build.doStop();
                 numCancels++;
