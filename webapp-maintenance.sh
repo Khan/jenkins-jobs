6b6282e2b64079d7feb3797811de06686d07f0d1
Craig Silverstein <csilvers@khanacademy.org>
2014-07-15 16:26:29 -0700

    Write a jenkins job to automatically compress our images once a week.
    Test Plan: Fingers crossed.  Will run manually once it's checked in.
    
    Reviewers: chris
    
    Reviewed By: chris
    
    Subscribers: alpert, marcos
    
    Differential Revision: http://phabricator.khanacademy.org/D11187

--- webapp-maintenance.sh
+++ webapp-maintenance.sh
@@ -1,2 +1,57 @@
-#!/bin/sh
+#!/bin/bash -xe
 
+# A Jenkins job that periodically runs some cleanup tasks on
+# our webapp.
+#
+# Here are some of the cleanups we run:
+#   deploy/pngcrush.py
+#       compress images
+#   TODO: khan-exercises/local-only/update_local.sh
+#       get khan-exercises matching webpp
+#   TODO: vacuum unused indexes
+#   TODO: store test times to use with the @tiny/@small/@large decorators
+#   TODO: remove obsolete webapp deploy-branches on github
+#
+# There are also some cleanups we'd like to run but probably can't
+# because they require manual intervention:
+#   tools/find_nltext_in_js
+#       find places in .js files that need $._(...)
+#   tools/list_unused_images
+#       find images we can delete from the repo entirely
+#   tools/list_unused_api_calls.py
+#       find /api/... routes we can delete from the repo entirely
+#   deploy/list_files_uploaded_to_appengine
+#       find files we can add to skip_files.yaml
+#   clean up translations that download_i18n.py's linter complains about
+#   clean up the bottom of lint_blacklist.txt
+#   move not-commonly-used s3 data to glacier
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+source "${SCRIPT_DIR}/build.lib"
+ensure_virtualenv
+
+# This lets us commit messages without a test plan
+export FORCE_COMMIT=1
+
+cd "$WEBSITE_ROOT"
+
+pngcrush() {
+    safe_pull .
+
+    deploy/pngcrush.py
+    {
+        echo "Automatic compression of webapp images via $0"
+        echo
+        echo "| size % | old size | new size | filename"
+        git status --porcelain | sort | while read status filename; do
+            old_size=`git show HEAD:"$filename" | wc -c`
+            new_size=`cat "$filename" | wc -c`
+            ratio=`expr $new_size \* 100 / $old_size`
+            echo "| $ratio% | $old_size | $new_size | $filename"
+        done
+    } | git commit -a -F -
+
+    safe_push .
+}
+
+pngcrush
