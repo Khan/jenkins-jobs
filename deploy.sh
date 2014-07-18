2d6754db3e4ca78fcfe4e00083fd6e70d7549547
Craig Silverstein <csilvers@khanacademy.org>
2014-07-18 11:33:07 -0700

    Bring deploy.sh into 2014 -- a pretty substantial rewrite.
    Summary:
    1. Add a new clean mode, some, which does what the old 'most' did.
    2. Allow setting of more deploy.py flags, such as --modules and --skip-i18n
    3. Let build.lib decrypt secrets instead of doing it ourselves.
    4. Use alert (from build.lib) rather than calling alertlib ourselves.
    5. Send hipchat messages as Mr Monkey, not Jenny Jenkins.
    6. Don't keep popping in and out of the webapp dir.  It's annoying.
    
    I changed the Makefile clean command to match what was here, which was
    better (using git submodule foreach).
    
    I also got rid of a commandline flag in deploy.py that wasn't being
    used anywhere anymore.
    
    I will have to change the deploy-znd jenkins job after this is in, to
    pass through the new flags (such as NO_I18N), and to change the
    default clean mode from 'most' to 'some'.
    
    Test Plan:
    Fingers way crossed.  I will do a znd deploy once this is checked in
    (as a way of testing this code!)
    
    Reviewers: chris
    
    Reviewed By: chris
    
    Differential Revision: http://phabricator.khanacademy.org/D11298

--- deploy.sh
+++ deploy.sh
@@ -23,17 +23,21 @@ fi
 # The AppEngine user to deploy as and the file containing the user's password.
 : ${DEPLOY_EMAIL:=prod-deploy@khanacademy.org}
 : ${DEPLOY_PW_FILE:="$HOME"/prod-deploy.pw}
-# The file containing the password to decrypt secrets.py.
-: ${SECRETS_PW_FILE:="$HOME"/secrets_py/secrets.py.cast5.password}
 
-# Set this to a non-empty value (e.g., 1) to prevent deploy.py from running
-# unit tests. It is the same as passing the "--no-tests" flag to deploy.py.
-: ${SKIP_TESTS:=}
+# These set various flags when calling deploy.py.  See also:
+#    VERSION: which sets --version
+#    CLEAN: which may set --no-clean
+#    NOTIFY_HIPCHAT: which may set --no-hipchat
+: ${MODULES:=}         # --modules: if set, a comma-separated list to deploy
+: ${SKIP_TESTS:=}      # --no-tests: set to "1" to append --no-tests
+: ${SKIP_I18N:=}       # --no-i18n: set to "1" to append --no-i18n
+: ${PRIME:=}           # --force-priming: set to "1" to append --force-priming
 
 # This controls how git cleans the working directory before running the build.
 # Valid values are:
 #   all  - Full clean that results in a pristine working copy.
-#   most - Reserve some generated files that deploy.py uses as a cache.
+#   some - Clean the workspaces (including .pyc files) but not genfiles.
+#   most - Clean the workspaces and genfiles, excluding js/ruby/python deps.
 #   none - Don't clean (this is the default so that devs don't lose work).
 : ${CLEAN:=none}
 
@@ -46,64 +50,77 @@ fi
 # The HipChat room notified on success, failure, and refusal to deploy.
 # NOTE: deploy.py notifications ignore this and always go to "1s and 0s".
 : ${HIPCHAT_ROOM:=HipChat Tests}
+: ${HIPCHAT_SENDER:=Mr Monkey}
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
 source "${SCRIPT_DIR}/build.lib"
 ensure_virtualenv
 
-# Clean out the working tree.
 
-pushd "$WEBSITE_ROOT"
-if [ "$CLEAN" = "all" ]; then
-    # We need to specify -f twice so deleted subrepos (things that
-    # used to be subrepos but aren't anymore) are deleted.  From the
-    # 'git clean' man page:
-    #    If an untracked directory is managed by a different git
-    #    repository, it is not removed by default...  Specify -f
-    #    twice to remove it.
-    git clean -qffdx
-    git submodule foreach git clean -qffdx
-elif [ "$CLEAN" = "most" ]; then
-    git clean -qffdx --exclude genfiles --exclude node_modules
-    # genfiles is excluded from "git clean" so we need to manually remove
-    # artifacts that should not be kept across builds.
-    rm -rf genfiles/test-reports genfiles/lint_errors.txt
-    git submodule foreach git clean -qfdx --exclude node_modules
-fi
-popd
+cd "$WEBSITE_ROOT"
+
+# Set up the flags we pass to deploy.py
+# We always set --no-up: Jenkins checks out the right revision for us.
+# The '|| true's are to work around the fact we are running bash -e.
+DEPLOY_FLAGS="--version=$DEPLOY_VERSION"
+DEPLOY_FLAGS="$DEPLOY_FLAGS --no-browser --no-up --clean-versions"
+[ -n "$MODULES" ] && DEPLOY_FLAGS="$DEPLOY_FLAGS --modules=$MODULES" || true
+[ -n "$SKIP_TESTS" ] && DEPLOY_FLAGS="$DEPLOY_FLAGS --no-tests" || true
+[ -n "$SKIP_I18N" ] && DEPLOY_FLAGS="$DEPLOY_FLAGS --no-i18n" || true
+[ -n "$PRIME" ] && DEPLOY_FLAGS="$DEPLOY_FLAGS --force-priming" || true
+[ "$NOTIFY_HIPCHAT" = "long" ] || DEPLOY_FLAGS="$DEPLOY_FLAGS --no-hipchat"
+
+# Clean out the working tree.
+case "$CLEAN" in
+    all)
+        "$MAKE" allclean
+        ;;
+    most)
+        "$MAKE" clean
+        # Be a bit more aggressive: delete un-git-added files, for instance.
+        git clean -qffdx --exclude genfiles --exclude node_modules
+        git submodule foreach git clean -qffdx --exclude node_modules
+        ;;
+    some)
+        git clean -qffdx --exclude genfiles --exclude node_modules
+        git submodule foreach git clean -qffdx --exclude node_modules
+        # genfiles is excluded from "git clean" so we need to manually
+        # remove artifacts that should not be kept across builds.
+        rm -rf genfiles/test-reports genfiles/lint_errors.txt
+        ;;
+    none)
+        ;;
+    *)
+        echo "Unknown value for CLEAN: '$CLEAN'"
+        exit 1
+esac
 
 # Run the deploy.
-
-( cd "$WEBSITE_ROOT" && "$MAKE" install_deps )
+"$MAKE" install_deps
 
 echo "Deploying"
-# We always set --no-up: Jenkins checks out the right revision for us.
-DEPLOY_FLAGS="--no-browser --no-up --clean-versions"
-if test -n "${SKIP_TESTS}"; then
-    DEPLOY_FLAGS+=" --no-tests"
-fi
-if [ "${NOTIFY_HIPCHAT}" != "long" ]; then
-    DEPLOY_FLAGS+=" --no-hipchat"
-fi
-pushd "$WEBSITE_ROOT"
-openssl cast5-cbc -d -in secrets.py.cast5 -out secrets.py -kfile "$SECRETS_PW_FILE"
-chmod 600 secrets.py
+
+# We need to deploy secrets.py to production, so it needs to be in
+# webapp/, not just in $SECRETS_DIR.
+decrypt_secrets_py_and_add_to_pythonpath
+cp -p "$SECRETS_DIR/secrets.py" .
+
 if ! python -u deploy/deploy.py $DEPLOY_FLAGS \
-    --version="$DEPLOY_VERSION" --email="$DEPLOY_EMAIL" --passin <"$DEPLOY_PW_FILE"
+    --email="$DEPLOY_EMAIL" --passin <"$DEPLOY_PW_FILE"
 then
-    if [ "${NOTIFY_HIPCHAT}" = "short" ]; then
-        echo "Oh no, the ${JOB_NAME} build is broken (sadpanda). Will a kind" \
-             "soul help me get back on my feet at ${BUILD_URL}console?" \
-             | third_party/alertlib-khansrc/alert.py --hipchat="$HIPCHAT_ROOM" --severity=error
+    if [ "$NOTIFY_HIPCHAT" = "short" ]; then
+        alert error \
+            "Oh no, the $JOB_NAME build is broken (sadpanda). Will a kind" \
+            "soul help me get back on my feet at ${BUILD_URL}console?" \
     fi
     exit 1
 fi
 
-LAST_COMMIT_SHORT=`git rev-parse --short HEAD`
-LAST_COMMIT_AUTHOR=`git log -n1 --format=format:"%ae"`
-if [ "${NOTIFY_HIPCHAT}" = "short" ]; then
-    echo "Just deployed <a href=\"https://khanacademy.kilnhg.com/Code/Website/Group/webapp/History/${LAST_COMMIT_SHORT}\">${LAST_COMMIT_SHORT}</a>" \
-         "by ${LAST_COMMIT_AUTHOR} to http://${DEPLOY_VERSION}.khan-academy.appspot.com" \
-         | third_party/alertlib-khansrc/alert.py --hipchat="$HIPCHAT_ROOM" --html --color=gray
+if [ "$NOTIFY_HIPCHAT" = "short" ]; then
+    LAST_COMMIT=`git rev-parse HEAD`
+    LAST_COMMIT_SHORT=`git rev-parse --short HEAD`
+    LAST_COMMIT_AUTHOR=`git log -n1 --format=format:"%ae"`
+    alert info \
+        "Just deployed <a href=\"https://github.com/Khan/webapp/commit/$LAST_COMMIT\">$LAST_COMMIT_SHORT</a>" \
+        "by $LAST_COMMIT_AUTHOR to http://$DEPLOY_VERSION.khan-academy.appspot.com" \
 fi
-popd
