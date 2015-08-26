4a6b5048c42c4fe90850c0b9e6652e868d38154d
Chris Klaiber <chris@khanacademy.org>
2015-08-26 14:26:26 -0700

    Remove unused file
    Summary:
    I've changed Jenkins to no longer call this file in update-translations,
    like the TODO says.
    
    Test Plan: Fingers crossed.
    
    Reviewers: csilvers, james
    
    Reviewed By: james
    
    Differential Revision: https://phabricator.khanacademy.org/D20963

--- checkin-updated-translations.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -xe
-
-# OBSOLETE: the functionality has moved back into update-translations.sh.
-#
-# TODO(csilvers): get rid of this file completely once we've changed
-# jenkins to no longer call it.
