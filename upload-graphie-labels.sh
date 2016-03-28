7fb945169c1dbce7defb01f3437abe7170d92f2d
Craig Silverstein <csilvers@khanacademy.org>
2016-03-27 18:31:22 -0700

    Change upload-graphie-labels to build one language at a time.
    upload-graphie-labels has been running out of memory on jenkins.  I
    think this is due to loading all the pofiles at once.  Hopefully
    switching to one per language will help.
    
    Auditors: emily
    
    Test Plan:
    Fingers crossed!  We'll see how the run goes tonight.

--- upload-graphie-labels.sh
+++ upload-graphie-labels.sh
@@ -21,10 +21,14 @@ safe_pull .
 # We also make sure the translations sub-repo is up to date.
 safe_pull intl/translations
 
-echo "Translating graphie labels."
 # upload_graphie_labels.py expects all of the labels to have already been
 # translated.
-kake/build_prod_main.py -v1 i18n_graphie_labels --language=all
+# TODO(csilvers): write a small script that does this instad.
+languages=`python -c 'import tools.appengine_tool_setup; tools.appengine_tool_setup.fix_sys_path(); import intl.locale; print "\n".join(intl.locale.all_locales_for_packages() - {"en"})'`
+for language in languages; do
+    echo "Translating graphie labels for $language."
+    kake/build_prod_main.py -v1 i18n_graphie_labels --language="$language"
+done
 
 echo "Uploading labels to S3."
 tools/upload_graphie_labels.py
