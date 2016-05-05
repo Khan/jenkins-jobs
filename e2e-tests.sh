9aee7d2ae367813c20277987adc7bd25bc2e99c1
Craig Silverstein <csilvers@khanacademy.org>
2016-05-05 14:28:06 -0700

    Run the android and website tests in parallel.
    Also document *all* the envvars that we use.
    
    Pair-programmed with nacho.
    
    Auditors: nacho
    
    Test plan:
    Fingers crossed!

--- e2e-tests.sh
+++ e2e-tests.sh
@@ -15,9 +15,13 @@
 # Whether to run a11y tests.
 : ${RUN_A11Y:=false}
 # Send an extra message to alert.py in the case of an error.
-: ${EXTRA_TEXT_ON_FAILURE:=""}  
+: ${EXTRA_TEXT_ON_FAILURE:=""}
 # "" to disable slack notifications.
 : ${SLACK_CHANNEL:=#1s-and-0s-deploys}
+# The jenkins build-url, used for debugging messages
+: ${BUILD_URL:=""}
+: ${WORKSPACE_ROOT:=.}
+: ${WEBSITE_ROOT:="$WORKSPACE_ROOT/webapp"}
 
 # Send a message to slack.
 # $1: the message to send
@@ -35,26 +39,36 @@ alert_slack() {
 # smiles, but not fail it and cause a rollback. If we exited with
 # failure here, it would be difficult to downgrade that.
 
-find end-to-end -name '*_e2etest.js'  \
-  | if [ "$RUN_A11Y" = "false" ]; then grep -v a11y; else cat; fi \
-  | xargs tools/end_to_end_webapp_testing.py --version $VERSION --no-colors --jobs=4 --engine=phantomjs \
-        || echo "end_to_end_webapp_testing.py exited with failure"
-
-if ! "$WORKSPACE_ROOT"/jenkins-tools/analyze_make_output.py \
-    --e2e_test_reports_file="$WEBSITE_ROOT"/genfiles/end_to_end_test_output.xml \
-    --jenkins_build_url="$BUILD_URL" \
-    --slack-channel="$SLACK_CHANNEL" 
-then
-    echo "analyze_make_output.py exited with failure"
-
-    if [ ! -z "$EXTRA_TEXT_ON_FAILURE" ]; then
-        alert_slack "$EXTRA_TEXT_ON_FAILURE" "warning"
+run_android_e2e_tests() {
+    if "$WORKSPACE_ROOT"/jenkins-tools/run_android_db_generator.sh; then
+        alert_slack "Mobile integration tests succeeded" "info"
+    else
+        echo "run_android_db_generator.sh exited with failure"
+        alert_slack "Mobile integration tests failed" "warning"
     fi
-fi
+}
 
-if "$WORKSPACE_ROOT"/jenkins-tools/run_android_db_generator.sh; then
-    alert_slack "Mobile integration tests succeeded" "info"
-else
-    echo "run_android_db_generator.sh exited with failure"
-    alert_slack "Mobile integration tests failed" "warning"
-fi
+run_website_e2e_tests() {
+    find end-to-end -name '*_e2etest.js'  \
+      | if [ "$RUN_A11Y" = "false" ]; then grep -v a11y; else cat; fi \
+      | xargs tools/end_to_end_webapp_testing.py --version $VERSION --no-colors --jobs=4 --engine=phantomjs \
+            || echo "end_to_end_webapp_testing.py exited with failure"
+
+    if ! "$WORKSPACE_ROOT"/jenkins-tools/analyze_make_output.py \
+        --e2e_test_reports_file="$WEBSITE_ROOT"/genfiles/end_to_end_test_output.xml \
+        --jenkins_build_url="$BUILD_URL" \
+        --slack-channel="$SLACK_CHANNEL"
+    then
+        echo "analyze_make_output.py exited with failure"
+        if [ ! -z "$EXTRA_TEXT_ON_FAILURE" ]; then
+            alert_slack "$EXTRA_TEXT_ON_FAILURE" "warning"
+        fi
+    fi
+}
+
+
+# We can run all these in parallel!  We move all output to stderr so
+# we don't have to worry about the output lines getting intermingled.
+run_website_e2e_tests 1>&2 &
+run_android_e2e_tests 1>&2 &
+wait
