d644bd177916ae5ae33d77dc7328bddc189008f0
Craig Silverstein <csilvers@khanacademy.org>
2016-06-10 17:15:57 -0700

    Have i18n-gcs-upload upload newly translated js/css files as well.
    Summary:
    Right now, i18n-gcs-upload uploads new .po files (actually .po.pickled
    and .index files) to GCS, so that the server can read them to get new
    translations of python/jinja2/other-server-side content.
    
    With this change, it now uploads the new translations of the .js and
    .css files to GCS as well, so that the server can server those new
    static files right away.
    
    There are a few tricky issues:
    
    1) While updating the .po files is always safe, when updating the
    .js/.css files we need to make sure that we're using a version of the
    js/css code that is compatible with what the current server code
    expects.
    
    2) We want to translate .js/.css files into a different set of
    languages that we want .po files for, conceivably.
    
    We implement (2) by listing all the languages we need packages for and
    filtering them out of the languages passed into this script.
    
    We implement (1) by making the callers always check out webapp to the
    git version that corresponds to the currently deployed code.  The
    callers can then update the intl/translations substate however they
    want, and then call this script.
    
    Here's what the caller to this script -- the i18n-gcs-upload jenkins
    job -- will look like once the script is in:
    ```
    [ -n "$LOCALES" ]
    
    git clone https://github.com/Khan/jenkins-tools 2>/dev/null || ( cd jenkins-tools && git pull )
    . jenkins-tools/build.lib
    safe_pull jenkins-tools   # gets submodules as well
    
    [ -n "$GAE_VERSION" ] || GAE_VERSION="`deploy/current_gae_version.py`"
    
    safe_sync_to_origin git@github.com:Khan/webapp "gae-$GAE_VERSION"
    (
    cd webapp/intl/translations
    _safe_fetch
    _safe_destructive_checkout "$TRANSLATIONS_COMMIT"
    )
    git commit -anm "$GAE_VERSION + intl/translations at $TRANSLATIONS_COMMIT"
    
    timeout -k 5m 23h env I18N_GCS_UPLOAD_LOCALES="$LOCALES" GAE_VERSION="$GAE_VERSION" jenkins-tools/i18n-gcs-upload.sh
    
    cd webapp
    git submodule update
    ```
    
    Test Plan:
    will monitor i18n-gcs-upload for a few runs after this is committed
    and the jenkins job is updated.
    
    Reviewers: james
    
    Reviewed By: james
    
    Subscribers: natalie
    
    Differential Revision: https://phabricator.khanacademy.org/D28113

--- i18n-gcs-upload.sh
+++ i18n-gcs-upload.sh
@@ -4,11 +4,22 @@
 # 1) build index and chunk files
 # 2) upload index and chunk files from genfiles/translations to a Google Cloud
 #    Storage bucket at ka_translations
-# 3) change the setting `translations_version`, which controls which set of
+# 3) build js and css files (and other files that we serve from GCS)
+# 4) Upload these static files to a Google Cloud Storage bucket at ka-static.
+# 5) change the Setting `translations_version`, which controls which set of
 #    translations the webapp will load from Google Cloud Storage
+# 6) change the Setting `js_css_md5sum`, which controls the set of js/css
+#    files that webapp will serve from Google Cloud Storage
 
 # This should be a space-separated list of locales.
 : ${I18N_GCS_UPLOAD_LOCALES:=}
+
+# If the webapp repo at WEBSITE_ROOT is synced to a commit that is
+# compatible with a current deployed gae version, this should be
+# set to that gae version.  In that case, we'll rebuild the js/css
+# files and upload them to prod, to replace the existing js/css at
+# that gae version.
+: ${GAE_VERSION:=}
 
 # empty string means 'default number of parallel jobs', otherwise it
 # should be an integer.
@@ -42,3 +53,22 @@ done
 # Convert the list of locales to '-l <locale> -l <locale> ...'
 locales_for_upload="-l `echo "$I18N_GCS_UPLOAD_LOCALES" | sed "s/ / -l /g"`"
 deploy/upload_gcs_i18n.py $locales_for_upload
+
+if [ -n "$GAE_VERSION" ]; then
+    # Let's build the js/css files as well, and update the Settings for them.
+    # But only do this if we've updated a language that we have js/css
+    # content for.
+    # TODO(csilvers): update deploy_to_gcs to take a list of languages, rather
+    # than all-or-nothing.
+    locales_with_packages="`intl/locale_main.py | sed -ne 's/locales for packages: //p' | tr ' ' '\012' | grep -vx en`"
+    locales_for_static_upload=""
+    for locale in $I18N_GCS_UPLOAD_LOCALES; do
+        if echo "$locales_with_packages" | grep -qx "$locale"; then
+            locales_for_static_upload="$locales_for_static_upload -l $locale"
+        fi
+    done
+
+    if [ -n "$locales_for_static_upload"]; then
+        deploy/deploy_to_gcs.py $JOBS "$GAE_VERSION"
+    fi
+fi
