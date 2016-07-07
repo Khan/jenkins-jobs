556c149384f27ebb1e02685d626ad5ea01c066df
Craig Silverstein <csilvers@khanacademy.org>
2016-07-07 14:57:24 -0700

    Add a script to ensure we send to slack on jenkins errors.
    Summary:
    This will run daily, so we can discover misconfigured jenkins jobs.
    
    With a few exceptions, we require all jenkins jobs to notify a slack
    room on failure.  Otherwise, we have no way of knowing when our jobs,
    which we think are working, actuallly are not.
    
    Test Plan:
    I ran the script manually except for the sending to slack, and saw
    that `$errors` had stuff like this:
    ```
    deploy-react-render-server: should send to slack on 'NotBuilt'
    deploy-react-render-server: should send to slack on 'Unstable'
    e2e-crawler-test: should send to slack on 'NotBuilt'
    e2e-test: should send to slack on 'NotBuilt'
    ```
    I fixed all those errors in jenkins and re-ran the script and saw that
    `$errors` is now empty.
    
    Reviewers: dhruv, alexanderforsyth
    
    Differential Revision: https://phabricator.khanacademy.org/D28854

---
+++ monitoring/check_slack_notifications.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# A script that verifies that all jenkins jobs notify slack on failure.
+#
+# With a few exceptions which we hard-code below, we require all
+# jenkins jobs to notify a slack room on failure.  Otherwise, we have
+# no way of knowing when our jobs, which we think are working,
+# actuallly are not.
+#
+# This is intended to be run as the ubuntu user on the jenkins machine.
+
+JENKINS_HOME=`grep ^jenkins: /etc/passwd | cut -d: -f6`
+
+# These are the jobs that are allowed not to send to slack for
+# whatever reason.  The format of this list is
+#    -e <jobname> -e <jobname> ...
+EXCEPTIONS="-e make-check-worker"
+
+errors=""
+
+error() {
+    errors="$errors\n$*"
+}
+
+for config in "$JENKINS_HOME"/jobs/*/config.xml; do
+    job=`dirname $config | xargs -n1 basename`
+
+    # Ignore whitelisted configs.
+    echo "$job" | grep -q -x $EXCEPTIONS && continue
+
+    # Ignore disabled configs
+    grep -q "<disabled>true</disabled>" "$config" && continue
+
+    # Make sure we send to slack on all the failure modes.
+    grep -q "<notifyAborted>true</notifyAborted>" "$config" || error "$job: should send to slack on 'Aborted'"
+    grep -q "<notifyNotBuilt>true</notifyNotBuilt>" "$config" || error "$job: should send to slack on 'NotBuilt'"
+    grep -q "<notifyUnstable>true</notifyUnstable>" "$config" || error "$job: should send to slack on 'Unstable'"
+    grep -q "<notifyFailure>true</notifyFailure>" "$config" || error "$job: should send to slack on 'Failure'"
+    grep -q "<room>#" "$config" || error "$job: must pick a slack channel to send to"
+    grep -q "</jenkins.plugins.slack.SlackNotifier>" "$config" || error "$job: you must enable slack in the *post-build actions* too"
+done
+
+if [ -n "$errors" ]; then
+    # We use /bin/echo to get well-defined behavior for treatment of '\n'
+    /bin/echo -e "Some of the Jenkins configs are not notifying slack on error: $errors" | \
+        env PYTHONPATH="$HOME/alertlib_secret" "$HOME/alertlib/alert.py" \
+            --slack "#infrastructure" \
+            --severity "error" \
+            --summary "Need to fix jenkins slack-notifications"
+fi
