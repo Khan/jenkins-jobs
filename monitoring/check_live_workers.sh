5126d0d638b9346f70c41be1cb18e3965a172906
Craig Silverstein <csilvers@khanacademy.org>
2016-07-26 17:00:10 -0700

    Change the script to only warn while we're actually running make-check.
    Summary:
    This should avoid all the false positives we've been getting due to
    worker machines starting and stopping at slightly different times (due
    to how long it takes tests to run on each machine).
    
    Test Plan: Fingers crossed
    
    Reviewers: dhruv
    
    Reviewed By: dhruv
    
    Subscribers: alexanderforsyth
    
    Differential Revision: https://phabricator.khanacademy.org/D29354

--- monitoring/check_live_workers.sh
+++ monitoring/check_live_workers.sh
@@ -1,54 +1,44 @@
 #!/bin/sh
 
-# A script that verifies that either all the jenkins worker machines are
-# online, or all are offline.
+# A script that verifies that all the jenkins worker machines are
+# online while `make check` (or allcheck) are running.
 #
-# We use jenkins workers for running tests.  We use a plugin that takes
-# all the worker machines offline when they've been idle for a while
-# (since we don't run tests all the time).  In that state we expect
-# all worker machines to be offline.  Otherwise, we expect them to all
-# be online.
+# We use jenkins workers for running tests.  We use a plugin that
+# takes all the worker machines offline when they've been idle for a
+# while (since we don't run tests all the time).  In that state we
+# expect all worker machines to be offline.  But when running tests,
+# we expect them to all be online.
 #
 # However, sometimes it happens that one or two machines becomes
 # offline even when they're supposed to be active, due to a kernel
 # oops or crash or some other problem.  In that case, we have to
 # manually restart the worker's ssh-connection from within jenkins.
 #
-# This script notices when that situation arises and is true for more
-# than 5 minutes, and sends a message to slack so that people can
-# handle it properly.
+# This script notices when that situation arises.  The way it works is
+# that the `make check` (and allcheck) jobs create a file in /tmp when
+# they start, and delete it when they end.  We check if that file
+# exists and is at least 5 minutes old (to give workers time to start
+# up).  If so, we require there to be 4 ssh connections to workers.
+# If not, we complain to slack so that people can handle it properly.
 #
 # This script is meant to be run as the `ubuntu` user.
 
+# How many minutes we wait for the jenkins workers to start up.
+GRACE_PERIOD=5
+
 JENKINS_HOME=`grep ^jenkins: /etc/passwd | cut -d: -f6`
-LIVE_WORKERS_FILE="/tmp/live_workers.is_bad"
-
-# We send a warning if failures have been going on since at least this time.
-A_WHILE_AGO=`date +%s -d "10 minutes ago"`
 
 
-warn() {
-    cat <<EOF | env PYTHONPATH="$HOME/alertlib_secret" \
-                      "$HOME/alertlib/alert.py" \
-                      --slack "#1s-and-0s-deploys,#infrastructure" \
-                      --severity "error" \
-                      --summary "Need to restart some jenkins workers"
-Some of the Jenkins worker machines are in a bad state and must be restarted.
+# Don't bother doing anything if make check hasn't been running for
+# at least $GRACE_PERIOD minutes.
+GT_GRACE_PERIOD=`expr "$GRACE_PERIOD" - 1`
+if [ -z "`find /tmp/make_check.run -mmin +"$GT_GRACE_PERIOD" 2>/dev/null`" ]; then
+    # File not found, or is younger than GT_GRACE_PERIOD.  We wait.
+    exit 0
+fi
 
-To do this, visit https://jenkins.khanacademy.org/computer.  Look in
-the right navigation pane for workers that are marked "(offline)".  If
-*all* of them are offline, that's expected and you're done.  If some
-are offline and some are online, on the other hand, click on each one
-that is offline and then click the big "Launch agent" button.
-
-(If you do not see the "Launch agent" button, you may lack the necessary
-permissions; ask for help on the #infrastructure slack room.)
-EOF
-}
-
-
-# First, figure out how many workers we have.  We can get that from
-# the jenkins configs.  We are looking for this section in config.xml:
+# Figure out how many workers we have.  We can get that from the
+# jenkins configs.  We are looking for this section in config.xml:
 # ```
 #           <instanceCap>4</instanceCap>
 #           <stopOnTerminate>true</stopOnTerminate>
@@ -70,29 +60,21 @@ expected_workers=`\
 #     without requiring sudo.  Maybe run this as the jenkins user?
 actual_workers=`sudo lsof -u jenkins -a -i :22 | grep ssh | wc -l`
 
-if [ "$actual_workers" -gt 0 -a "$actual_workers" -lt "$expected_workers" ]; then
-    # OK, let's see if this problem has been going on for a while.
-    # The idea is we create LIVE_WORKERS_FILE every time we see a
-    # problem and the file doesn't already exist, and delete it every
-    # time we don't see a problem.  So if LIVE_WORKERS_FILE is at least
-    # X minutes old, that means we've been failing for >=X minutes.
-    if [ -e "$LIVE_WORKERS_FILE" ]; then
-        if [ `stat -c %Y "$LIVE_WORKERS_FILE"` -le "$A_WHILE_AGO" ]; then
-            warn
-            # Make it so that we don't warn again for a little while.
-            rm -f "$LIVE_WORKERS_FILE"
-        else
-            # Failures have been going on, but not long enough for us to
-            # warn.  Let's just chill until next time.
-            :
-        fi
-    else
-        # We're failing, but LIVE_WORKERS_FILE doesn't exist.  That
-        # means we're the first failure, so let's make a note of it!
-        # We won't warn yet though; it's too early.
-        touch "$LIVE_WORKERS_FILE"
-    fi
-else
-    # Everything is ok, let's delete the "it's bad" file.  Happy times!
-    rm -f "$LIVE_WORKERS_FILE"
+if [ "$actual_workers" -lt "$expected_workers" ]; then
+    cat <<EOF | env PYTHONPATH="$HOME/alertlib_secret" \
+                      "$HOME/alertlib/alert.py" \
+                      --slack "#1s-and-0s-deploys,#infrastructure" \
+                      --severity "error" \
+                      --summary "Need to restart some jenkins workers"
+Some of the Jenkins worker machines are in a bad state and must be restarted.
+
+To do this, visit https://jenkins.khanacademy.org/computer.  Look in
+the right navigation pane for workers that are marked "(offline)".  If
+*all* of them are offline, that's expected and you're done.  If some
+are offline and some are online, on the other hand, click on each one
+that is offline and then click the big "Launch agent" button.
+
+(If you do not see the "Launch agent" button, you may lack the necessary
+permissions; ask for help on the #infrastructure slack room.)
+EOF
 fi
