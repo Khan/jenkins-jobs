1b8f0855b5968b0980264c7100a6a572d585ac94
Dhruv Kapadia <dhruv@khanacademy.org>
2016-08-02 10:16:42 -0400

    Pass option for selenium test to buffer output
    Summary:
    In D29573, we changed the selenium test runner so that stdout and stderr
    will not be buffered by default. Once that change is deployed, we'll
    need to add this flag to make sure that output is buffered in CI
    
    Depends on: D29573
    
    Test Plan: will run e2e tests to confirm this works
    
    Reviewers: johnsullivan
    
    Reviewed By: johnsullivan
    
    Subscribers: csilvers
    
    Differential Revision: https://phabricator.khanacademy.org/D29574

--- e2e-tests.sh
+++ e2e-tests.sh
@@ -82,6 +82,7 @@ run_selenium_e2e_tests() {
         # pwd needs to be set to webapp for selenium_webapp_testing to be set
         if ! (cd $WEBSITE_ROOT;
               "$WEBSITE_ROOT/tools/selenium_webapp_testing.py" selenium_tests \
+              --buffer-output \
               --url "$URL" --driver sauce --jobs 4 --retries 3 \
               --xml-dir "$WEBSITE_ROOT/genfiles/selenium_test_reports/" )
         then
