c3662ca6fad14aae5f4cc07cd05d64600ae296c6
Craig Silverstein <csilvers@khanacademy.org>
2017-01-27 15:35:37 -0800

    It turns out I don't want to use classes after all.
    An obscure note in
       https://jenkins.io/doc/book/pipeline/shared-libraries/
    indicates you can't call steps like `sh` from class methods, you have
    to use global methods.  Apparently these are a thing in groovy, though
    they're not well documented.
    
    Auditors: colin, amos, chris
    
    Test plan:
    Fingers crossed

--- src/org/khanacademy/GitUtils.groovy
+++ src/org/khanacademy/GitUtils.groovy
@@ -3,19 +3,21 @@
 package org.khanacademy;
 
 
+// Turn a list of submodules into arguments to pass to build.lib functions.
 // Submodules is the empty list (default) for "clone all submodules".
 // Submodules is null for "clone no submodules".
 // Otherwise it's a list of prefixes; we clone all submodules matching
 //     some prefix.
-class GitUtils {
-    static def safeSyncToOrigin(repoToClone, commit, submodules=[]) {
-        def submodulesString = '';
-        if (submodules == null) {
-           submodulesString = 'no_submodules';
-        } else {
-           submodulesString = submodules.join(' ');
-        }
-        sh ("jenkins-tools/build.lib safe_sync_to_origin " +
-            "${repoToClone} ${commit} ${submodulesString}");
-    }
+def _submodulesArg(submodules) {
+   if (submodules == null) {
+      return 'no_submodules';
+   } else {
+      return submodules.join(' ');
+   }
+}
+
+// Submodules is as in _submodulesArg.
+def safeSyncToOrigin(repoToClone, commit, submodules=[]) {
+   sh ("jenkins-tools/build.lib safe_sync_to_origin " +
+       "${repoToClone} ${commit} ${_submodulesArg(submodules)}");
 }
