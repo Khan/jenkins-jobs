f6cdfa42ab16cab930265891efc5f70b21f7bf1d
Craig Silverstein <csilvers@khanacademy.org>
2017-01-30 22:00:28 -0800

    sh doesn't support pushd/popd, but it does support `cd -`.
    `sh` is `dash` on linux.  I thought pushd/popd were POSIX, but I guess
    not.  I don't think `cd -` is either but luckily dash supports it.
    It's all the same thing.
    
    Auditors: colin, chris, amos
    
    Test plan:
    Fingers crossed

--- vars/pip.groovy
+++ vars/pip.groovy
@@ -1,8 +1,8 @@
 def call(shellCommand, installSecrets=false, workspaceRoot="..") {
-   sh ("pushd '${workspaceRoot}'; " +
+   sh ("cd '${workspaceRoot}'; " +
        ". ./jenkins-tools/build.lib; " +
        "ensure_virtualenv; " +
        "[ '${installSecrets}' = 'true' ] && decrypt_secrets_py_and_add_to_pythonpath; " +
-       "popd; " +
+       "cd -; " +
        "${shellCommand}");
 }
