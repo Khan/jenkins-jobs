0f41bdaa4dcc2ed4712061a970f743ad33eb7786
Craig Silverstein <csilvers@khanacademy.org>
2017-01-30 21:40:49 -0800

    Don't try to muck with timestamp command in `pip`.
    It turns out if you have two nested timestamp calls, it prints out the
    time twice on every output line.  That's not great.  If we want to
    make sure exactly one timestamp is active at a time, it's best to do
    it at a higher level (at `node` or maybe even globally if that works),
    then at a low level here.
    
    Auditors: colin, chris, amos
    
    Test plan:
    None

--- vars/pip.groovy
+++ vars/pip.groovy
@@ -1,10 +1,8 @@
 def call(shellCommand, installSecrets=false, workspaceRoot="..") {
-   timestamps {
-      withEnv(["WORKSPACE_ROOT=${workspaceRoot}"]) {
-         sh (". $WORKSPACE_ROOT/jenkins-tools/build.lib; " +
-             "ensure_virtualenv; " +
-             "[ '${installSecrets}' = 'true' ] && decrypt_secrets_py_and_add_to_pythonpath; " +
-             "${shellCommand}");
-      }
+   withEnv(["WORKSPACE_ROOT=${workspaceRoot}"]) {
+      sh (". $WORKSPACE_ROOT/jenkins-tools/build.lib; " +
+          "ensure_virtualenv; " +
+          "[ '${installSecrets}' = 'true' ] && decrypt_secrets_py_and_add_to_pythonpath; " +
+          "${shellCommand}");
    }
 }
