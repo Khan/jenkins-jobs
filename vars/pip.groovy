e659b137e261978559fe8a6897507836c530b2e1
Craig Silverstein <csilvers@khanacademy.org>
2017-01-30 21:44:48 -0800

    It turns out you need to source build.lib from WORKSPACE_ROOT.
    I thought you could just set WORKSPACE_ROOT and build.lib would do the right
    things even if you sourced it from another directory, but that doesn't
    work.  It's possible the only problem is the `git rev-parse` sanity
    check, so I *could* just fix that, but it seems safest to go the
    tested route and just do a `cd` before sourcing build.lib.  Then I can
    get rid of the withEnv call as well.
    
    Auditors: chris, amos, colin
    
    Test plan:
    Fingers crossed

--- vars/pip.groovy
+++ vars/pip.groovy
@@ -1,8 +1,8 @@
 def call(shellCommand, installSecrets=false, workspaceRoot="..") {
-   withEnv(["WORKSPACE_ROOT=${workspaceRoot}"]) {
-      sh (". $WORKSPACE_ROOT/jenkins-tools/build.lib; " +
-          "ensure_virtualenv; " +
-          "[ '${installSecrets}' = 'true' ] && decrypt_secrets_py_and_add_to_pythonpath; " +
-          "${shellCommand}");
-   }
+   sh ("pushd '$WORKSPACE_ROOT'; " +
+       ". ./jenkins-tools/build.lib; " +
+       "ensure_virtualenv; " +
+       "[ '${installSecrets}' = 'true' ] && decrypt_secrets_py_and_add_to_pythonpath; " +
+       "popd; " +
+       "${shellCommand}");
 }
