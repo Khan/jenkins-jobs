62b5a27d6c2827459b9100e7cb7a588abb153b47
Craig Silverstein <csilvers@khanacademy.org>
2017-01-30 21:56:09 -0800

    OMG variables what are they?!?
    Auditors: colin chris amos
    
    Test plan:
    Hope.

--- vars/pip.groovy
+++ vars/pip.groovy
@@ -1,5 +1,5 @@
 def call(shellCommand, installSecrets=false, workspaceRoot="..") {
-   sh ("pushd '$WORKSPACE_ROOT'; " +
+   sh ("pushd '${workspaceRoot}'; " +
        ". ./jenkins-tools/build.lib; " +
        "ensure_virtualenv; " +
        "[ '${installSecrets}' = 'true' ] && decrypt_secrets_py_and_add_to_pythonpath; " +
