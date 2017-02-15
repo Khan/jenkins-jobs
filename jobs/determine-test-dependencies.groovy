ce1332273fc3ff95e3d87430ccb88006481ab39e
Craig Silverstein <csilvers@khanacademy.org>
2017-02-15 14:25:37 -0800

    Comment out setCronSchedule, it causes a hang.
    I don't know why.  The easiest solution may be to move Setup from
    `class` to `vars` like everyone else.
    
    Auditors: benkraft
    
    Test plan:
    Be sad.

--- jobs/determine-test-dependencies.groovy
+++ jobs/determine-test-dependencies.groovy
@@ -46,8 +46,10 @@ the <code>ka-test worker</code> ec2 setup at
 to click on 'advanced' to see the instance cap.""",
    "4"
 
-).setCronSchedule(
-   'H 3 * * *'
+// TODO(csilvers): re-enable once I figure out why setCronSchedule causes
+// our process to hang.
+//).setCronSchedule(
+//   'H 3 * * *'
 
 ).apply();
 
