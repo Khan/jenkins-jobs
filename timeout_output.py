84c20bf8abed796b8616b3d231cff825641f41cd
Craig Silverstein <csilvers@khanacademy.org>
2017-02-15 15:39:55 -0800

    Don't add p.stdin to write_set after it's been closed.
    This is just an efficiency thing, it doesn't matter for correctness,
    but there's no need to see if a closed file is going to be writable
    because it never is!
    
    Auditors: amos
    
    Test plan:
    Ran
       cat /etc/ca-certificates.conf | ../jenkins-tools/timeout_output.py 45m cat | wc -c
    and saw it say `9112`, which is the size of that file.

--- timeout_output.py
+++ timeout_output.py
@@ -88,8 +88,8 @@ def main(cmd_list, duration, signum, kill_after):
             else:
                 if not p.stdin.closed:
                     input += data
-                if p.stdin not in write_set:
-                    write_set.append(p.stdin)
+                    if p.stdin not in write_set:
+                        write_set.append(p.stdin)
                 continue
 
         if p.stdin in wlist:
