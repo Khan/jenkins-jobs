d1243cd5a715c35fac797924013aba837ed8293a
Craig Silverstein <csilvers@khanacademy.org>
2017-02-27 14:14:55 -0800

    Convert the i18n-gcs-upload script to a groovy script.
    Summary:
    The translation (ha!) was fairly straightforward.  However, the script
    itself seemed to have suffered from some bitrot: it had both
    GAE_VERSION and GIT_TAG but only used the former.  But then the script
    it called (i18n-gcs-upload.sh) was expecting the latter!  God knows
    if this script has been working right.
    
    Now we just define GIT_TAG and use it everywhere.
    
    Test Plan:
    Fingers crossed -- will deploy this and try running it manually, then
    change the i18n-download-translations jenkins job to run this one
    rather than the old version.
    
    Reviewers: james
    
    Reviewed By: james
    
    Subscribers: colin, chris, amos
    
    Differential Revision: https://phabricator.khanacademy.org/D33796

---
+++ jobs/i18n-gcs-upload.groovy
@@ -0,0 +1,121 @@
+// The pipeline job to upload i18n .index/.chunk files to GCS.
+// .index/.chunk files are our own processed form of .mo files,
+// used for translation.  We upload these to GCS, where webapp
+// accesses them when it needs to translate some content at
+// runtime.  (This is for datastore content and the occassional
+// python string.)
+//
+// This job is called automatically every time we download content
+// from crowdin.
+
+
+@Library("kautils")
+// Classes we use, under jenkins-tools/src/.
+import org.khanacademy.Setup;
+// Vars we use, under jenkins-tools/vars/.  This is just for documentation.
+//import vars.exec
+//import vars.kaGit
+//import vars.notify
+//import vars.onMaster
+
+
+new Setup(steps
+
+).blockBuilds(["builds-using-a-lot-of-memory"]
+
+).addStringParam(
+    "LOCALES",
+    """A whitespace-separated list of locales to upload to GCS.
+<b>required</b>""",
+    ""
+
+).addStringParam(
+    "GIT_TAG",
+    """The git tag corresponding to the release that we should build our
+files against.  (But see TRANSLATIONS_COMMIT, below.) This needs to be
+one of the `gae-*` tags, not an arbitrary git commit-ish, because we
+parse the static-content version out of it.""",
+    ""
+
+).addStringParam(
+    "TRANSLATIONS_COMMIT",
+    """The git commit-ish to sync intl/translations subdirectory to when
+building our files.  This means that when we build our genfiles -- the
+pofiles and the js/css files -- we are doing so in a hybrid
+\"frankenfile\" mode, with the source code as specified by
+GAE_VERSIONS, but with the translations as specified by
+TRANSLATIONS_COMMIT.""",
+    "origin/master"
+
+).apply();
+
+
+def syncRepos() {
+   onMaster("10m") {
+      def gitTag = params.GIT_TAG;
+      if (!gitTag) {
+         // We sync to master so we can get the git-tag of the current
+         // live version of webapp.  Then we sync to that.
+         kaGit.safeSyncTo("git@github.com:Khan/webapp", "master");
+         dir("webapp") {
+            gitTag = sh(script: "deploy/current_version.py --git-tag",
+                        returnStdout: true).trim();
+         }
+      }
+      kaGit.safeSyncTo("git@github.com:Khan/webapp", gitTag);
+
+      dir("webapp/intl/translations") {
+         // TODO(csilvers): don't use build.lib internals.
+         exec(["../../../jenkins-tools/build.lib",
+               "_safe_fetch"]);
+         exec(["../../../jenkins-tools/build.lib",
+               "_safe_destructive_checkout", params.TRANSLATIONS_COMMIT]);
+      }
+   }
+}
+
+
+def runScript() {
+   onMaster("23h") {
+      withEnv(["GIT_TAG=${params.GIT_TAG}",
+               "I18N_GCS_UPLOAD_LOCALES=${params.LOCALES}"]) {
+         sh("jenkins-tools/i18n-gcs-upload.sh");
+      }
+   }
+}
+
+
+def resetRepo() {
+   onMaster("10m") {
+      dir("webapp") {
+         sh("git submodule update");
+      }
+   }
+}
+
+
+// TODO(csilvers): update the slack message with the updated locales.
+notify([slack: [channel: '#i18n',
+                when: ['FAILURE', 'UNSTABLE']]]) {
+   // Make sure LOCALES was specified -- it's an error not to list a
+   // locale to update!
+   if (!params.LOCALES) {
+      error("You must specify at least one locale to upload!");
+   }
+
+   stage("Syncing repos") {
+      syncRepos();
+   }
+
+   try {
+      stage("Running script") {
+         runScript();
+      }
+   } finally {
+      // Just to be nice -- it's not essential -- let's reset the repo
+      // back to normal.
+      stage("Resetting repo") {
+         resetRepo();
+      }
+   }
+}
