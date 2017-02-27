b88a8c936bd23c1f77eb03c03dc42621a92b6dd0
Craig Silverstein <csilvers@khanacademy.org>
2017-02-26 16:11:27 -0800

    Change the failure text to say how often the job has been failing.
    This seems like a useful thing to know.  It is also a first step
    toward supporting "BACK TO NORMAL".
    
    Auditors: colin, chris, amos
    
    Test plan:
    This gave no errors:
       groovy vars/notify.groovy
    
    If it fails, it will be after the actual job finishes, so I feel
    pretty comfortable just pushing this.

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -3,24 +3,51 @@
 //import vars.onMaster
 //import vars.withSecrets
 
+// Number of jobs that have failed in a row, including this one.
+def _numConsecutiveFailures() {
+   def numFailures = 0;
+   def build = currentBuild;    // a global provided by jenkins
+   while (build && build.result == "FAILURE") {
+      numFailures++;
+      build = build.previousBuild;
+   }
+   return numFailures;
+}
+
+def _ordinal(num) {
+   if (num % 100 == 11 || num % 100 == 12 || num % 100 == 13) {
+      return num.toString() + "th";
+   }
+   def suffix = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"];
+   return num.toString() + suffix[num % 10];
+}
+
+
+def _statusText(status) {
+   if (status == "FAILURE") {
+      def numFailures = _numConsecutiveFailures();
+      if (numFailures > 1) {
+         return "failed (${_ordinal(numFailures)} time in a row)";
+      }
+      return "failed";
+   } else if (status == "UNSTABLE") {
+      return "is unstable";
+   } else if (status == "SUCCESS") {
+      return "succeeded";
+   } else {
+      return "has unknown status (oops!)";
+   }
+}
+
+
 def sendToSlack(slackOptions, status) {
    if (!(status in slackOptions.when)) {
       return;
    }
-   onMaster('1m') {
+   onMaster("1m") {
       withSecrets() {     // you need secrets to talk to slack
-         if (status == 'FAILURE') {
-            status = 'failed';
-         } else if (status == 'UNSTABLE') {
-            status = 'is unstable';
-         } else if (status == 'SUCCESS') {
-            status = 'succeeded';
-         } else {
-            status = 'has unknown status (oops!)';
-         }
-
-         def msg = ("${env.JOB_NAME} ${currentBuild.displayName} ${status} " +
-                    "(<${env.BUILD_URL}|Open>)");
+         def msg = ("${env.JOB_NAME} ${currentBuild.displayName} " +
+                    "${_statusText(status)} (<${env.BUILD_URL}|Open>)");
          sh("echo ${exec.shellEscape(msg)} | " +
             "jenkins-tools/alertlib/alert.py " +
             "--slack=${exec.shellEscape(slackOptions.channel)} " +
@@ -38,11 +65,11 @@ def sendToEmail(emailOptions, status) {
 
 
 def call(options, Closure body) {
-   def status = 'SUCCESS';
+   def status = "SUCCESS";
    try {
       body();
    } catch (e) {
-      status = 'FAILURE';
+      status = "FAILURE";
       throw e;
    } finally {
       if (currentBuild.result != null) {
