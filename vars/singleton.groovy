5a9e7c31af6e107d8b236daf5e732bf89e7174f7
Craig Silverstein <csilvers@khanacademy.org>
2017-02-28 11:39:17 -0800

    Oops, we need to run our sh commands on a node...
    Auditors: amos, chris, colin
    
    Test plan:
    Fingers crossed.  `groovy vars/singleton.groovy` passed, at least.

--- vars/singleton.groovy
+++ vars/singleton.groovy
@@ -1,15 +1,20 @@
+// We use these user-defined steps from vars/:
+//import vars.onMaster
+
 def call(key, Closure body) {
    if (key == null) {
       body();
       return;
    }
 
-   def cacheValue = sh(script: "redis-cli --raw GET '${key}'",
-                       returnStdout: true).trim();
-   // A cache hit!  We don't need to do any work.
-   if (cacheValue == "1") {
-      echo "Cache hit on '${key}' -- not running the body of this job.";
-      return;
+   onMaster('1m') {
+      def cacheValue = sh(script: "redis-cli --raw GET '${key}'",
+                          returnStdout: true).trim();
+      // A cache hit!  We don't need to do any work.
+      if (cacheValue == "1") {
+         echo "Cache hit on '${key}' -- not running the body of this job.";
+         return;
+      }
    }
 
    body();
@@ -18,6 +23,8 @@ def call(key, Closure body) {
    // was a success).  But we check explicitly for currentBuild.status
    // anyway in case the user explicitly set the status to not-success.
    if (currentBuild.status == null || currentBuild.status == "SUCCESS") {
-      sh("redis-cli SET '${key}' '1'");
+      onMaster('1m') {
+         sh("redis-cli SET '${key}' '1'");
+      }
    }
 }
