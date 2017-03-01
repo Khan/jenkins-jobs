9c33f58fc6c054447ad5deac8ead4a00caef2d60
Craig Silverstein <csilvers@khanacademy.org>
2017-03-01 13:03:50 -0800

    Doh, forgot the `script:` keyword!
    Auditors: colin, amos, chris
    
    Test plan:
    Fingers crossed

--- vars/singleton.groovy
+++ vars/singleton.groovy
@@ -9,8 +9,9 @@ def call(key, Closure body) {
    }
 
    onMaster('1m') {
-      def cacheValue = sh("redis-cli --raw GET ${exec.shellEscape(key)}",
-                          returnStdout: true).trim();
+      def cacheValue = sh(
+         script: "redis-cli --raw GET ${exec.shellEscape(key)}",
+         returnStdout: true).trim();
       // A cache hit!  We don't need to do any work.
       if (cacheValue == "1") {
          echo "Cache hit on '${key}' -- not running the body of this job.";
