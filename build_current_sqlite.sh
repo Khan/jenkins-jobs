3c6060c26d5aed5a527893c615223c77d5140240
Craig Silverstein <csilvers@khanacademy.org>
2017-03-01 17:40:38 -0800

    Move the main boddy of the build-current-sqlite jenkins job to a shell script.
    Summary:
    This is following the lead of other jobs like i18n-gcs-upload and
    sync-captions.  It is also much easier, in a groovy world, to call
    out to a shell script than have a ton of `sh` commands inside groovy.
    
    This isn't used yet, but once it's checked in I'll change the existing
    build-current-sqlite job to just call out to it.
    
    My only changes were to add double-quotes in lots of places, and to
    change the formatting a bit (4-space indents, etc).
    
    Test Plan: Fingers crossed.
    
    Reviewers: amos
    
    Reviewed By: amos
    
    Differential Revision: https://phabricator.khanacademy.org/D33904

---
+++ build_current_sqlite.sh
@@ -0,0 +1,80 @@
+#!/bin/sh -ex
+
+# A script to build an up-to-date current.sqlite by using the
+# run_sync.sh tool.  We download the current datastore snapshots from
+# gcs, run the tool to create a current.sqlite, and upload that to
+# gcs.
+#
+# For this script to work, secrets.py must be on the PYTHONPATH.
+
+
+# Space-separated list of GCS buckets to download snapshots from.
+: ${SNAPSHOT_BUCKETS:="snapshot_en snapshot_es"}
+# GCS bucket to upload current.sqlite to.
+: ${CURRENT_SQLITE_BUCKET:="gs://ka_dev_sync/current.sqlite"}
+
+
+# fetch sync snapshots
+for snapshot_bucket in $SNAPSHOT_BUCKETS; do
+    gsutil cp "gs://ka_dev_sync/$snapshot_bucket" "./$snapshot_bucket"
+done
+
+# remove any existing database
+rm -f current.sqlite
+
+# make deps
+cd webapp
+make deps
+
+
+for snapshot_bucket in $SNAPSHOT_BUCKETS; do
+    # start dev server serving locally on port 9085
+    # write logs to genfiles/appserver.log
+    timeout 10h python third_party/frankenserver/dev_appserver.py \
+        --application=khan-academy \
+        --datastore_path=../current.sqlite \
+        --port=9085 \
+        --require_indexes=yes \
+        --skip_sdk_update_check=yes \
+        --automatic_restart=no \
+        --admin_port=0 \
+        --max_module_instances=1 \
+        --log_level=info \
+        --host=127.0.0.1 \
+        ./app.yaml > genfiles/appserver.log 2>&1 &
+    appserver_pid=$!
+
+    # wait for server to start
+    sleep 30
+
+    # do sync
+    locale_name=`echo "$snapshot_bucket" | awk -F"_" '{print $NF}'`
+    tools/devshell.py --host localhost:9085 \
+        --script content/run_sync.py "../$snapshot_bucket" "$locale_name"
+    tools/devshell.py --host localhost:9085 \
+        --script tools/update_translation_analytics_data.py "$locale_name"
+    sleep 10
+
+    # stop dev server
+    kill -15 "$appserver_pid"
+    sleep 3
+    if ps -p "$appserver_pid" > /dev/null; then
+        kill -15 "$appserver_pid"
+        sleep 3
+    fi
+    if ps -p "$appserver_pid" > /dev/null; then
+        kill -9 "$appserver_pid"
+        sleep 3
+    fi
+done
+
+
+# upload current.sqlite
+cd ..
+gsutil cp current.sqlite "$CURRENT_SQLITE_BUCKET"
+
+# cleanup
+rm -f current.sqlite
+for snapshot_bucket in $SNAPSHOT_BUCKETS; do
+    rm -f "$snapshot_bucket"
+done
