05cbbc60af8b663749d0f45a65dabd04ff8277b0
Craig Silverstein <csilvers@khanacademy.org>
2017-03-03 13:31:04 -0800

    Groovy version of the sync-captions job.
    Summary:
    This job does everything in a single shell script, so it was easy
    enough to move over!
    
    I changed it to tell slack on success.  I figure it feels good to know
    it works, and it only runs twice a week.
    
    And -- since there's always something new -- this will be my chance to
    play around with the `PATH+whatever=xx` functionality to prepend xx to
    $PATH.
    
    Test Plan:
    I ran
       groovy jobs/sync-captions.groovy
    
    But I'll try to run this as well once it's committed.
    
    Reviewers: james
    
    Reviewed By: james
    
    Subscribers: colin, chris, amos
    
    Differential Revision: https://phabricator.khanacademy.org/D33976

---
+++ jobs/sync-captions.groovy
@@ -0,0 +1,53 @@
+// Pipeline job to download captions from Amara, then upload them to YouTube.
+
+@Library("kautils")
+// Classes we use, under jenkins-tools/src/.
+import org.khanacademy.Setup;
+// Vars we use, under jenkins-tools/vars/.  This is just for documentation.
+//import vars.exec
+//import vars.kaGit
+//import vars.notify
+//import vars.onMaster
+
+
+new Setup(steps
+
+).addCronSchedule("H 3 * * 3,6"
+
+).addStringParam(
+    "SKIP_TO_STAGE",
+    """Skip some stages of the sync.
+<ul>
+  <li>Stage 0 = Download from dropbox. </li>
+  <li>Stage 1 = Move professional translations into incoming folder.</li>
+  <li>Stage 2 = Download from Amara.</li>
+  <li>Stage 3 = Upload to Youtube.</li>
+  <li>Stage 4 = Upload to production.</li>
+p</ul>""",
+    ""
+
+).apply();
+
+
+def runScript() {
+   onMaster('23h') {
+      kaGit.safeSyncTo("git@github.com:Khan/webapp", "master");
+      withEnv(["SKIP_TO_STAGE=${params.SKIP_TO_STAGE}",
+               // Needed to get appengine_tool_setup.py
+               "PYTHONPATH+TOOLS=${pwd()}/webapp/tools"]) {
+         sh("jenkins-tools/sync-captions.sh");
+      }
+   }
+}
+
+
+notify([slack: [channel: '#i18n',
+                sender: 'Mr Monkey',
+                emoji: ':monkey_face:',
+                when: ['SUCCESS', 'FAILURE', 'UNSTABLE', 'ABORTED']],
+        email: [to: 'jenkins-admin+builds, james',
+                when: ['BACK TO NORMAL', 'FAILURE', 'UNSTABLE']]]) {
+   stage("Syncing captions") {
+      runScript();
+   }
+}
