257c23980cf01c0553e4fbc56f96abb90496269c
Craig Silverstein <csilvers@khanacademy.org>
2017-03-09 10:43:30 -0800

    Revert "Revert "Revert "Another try to use the Notify class."""
    This reverts commit 7dcab4072c74dc8c3f9d82548b018fa78c792734.

--- src/org/khanacademy/FailedBuild.groovy
+++
@@ -1,7 +0,0 @@
-// A new exception type, used in vars/notify.groovy.
-
-package org.khanacademy;
-
-// Used to set status to FAILED and emit the failure reason to slack/email.
-public class FailedBuild extends Exception {
-};
