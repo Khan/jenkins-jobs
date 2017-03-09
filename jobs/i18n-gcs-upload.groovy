2b53a2d9c1f5ab79b57c8125582c1f50b43641e0
Craig Silverstein <csilvers@khanacademy.org>
2017-03-09 09:34:01 -0800

    I knew it was too much to hope for my outputOf change would be error-free.
    Auditors: james
    
    Test plan:
    Fingers crossed

--- jobs/i18n-gcs-upload.groovy
+++ jobs/i18n-gcs-upload.groovy
@@ -78,7 +78,7 @@ def syncRepos() {
             // We want the last release that's successfully been tagged.
             // This can differ from the current release when a deploy
             // is going on (after deploying to GAE and before finishing).
-            gitTag = exec.outputOf("deploy/git_tags.py").split("\n")[-1];
+            gitTag = exec.outputOf(["deploy/git_tags.py"]).split("\n")[-1];
          }
       }
       kaGit.safeSyncTo("git@github.com:Khan/webapp", gitTag);
