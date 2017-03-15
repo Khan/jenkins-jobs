9989fc8cfa06af79f7067caf330415edb11d7cd6
James Irwin <james@khanacademy.org>
2017-03-14 22:04:08 -0700

    Make sure we are in webapp
    Test Plan:
    cross-fingers
    
    Auditors: csilvers

--- jobs/i18n-download-translations.groovy
+++ jobs/i18n-download-translations.groovy
@@ -37,9 +37,12 @@ def runScript() {
          def overrideLangs = params.LOCALES;
 
          if (!overrideLangs) {
-            // If not passed in as a param, get the single highest priority lang
-            overrideLangs = exec.outputOf(["deploy/order_download_i18n.py", 
-                                           "--verbose"]).split("\n")[0]
+             dir("webapp") {
+                // If not passed in as a param, get the single highest priority 
+                // lang
+                overrideLangs = exec.outputOf(["deploy/order_download_i18n.py", 
+                                               "--verbose"]).split("\n")[0]
+             }
          }
 
          currentBuild.displayName = ("${currentBuild.displayName} " +
