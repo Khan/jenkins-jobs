a78f562a418bc401cf911aa2b674a5d29b8a9f44
James Irwin <james@khanacademy.org>
2017-03-14 22:17:58 -0700

    Install tabulate.
    I would have thought requirements.txt is installed but, it seems not.
    
    Test Plan:
    cross-fingers
    
    Auditors: csilvers

--- jobs/i18n-download-translations.groovy
+++ jobs/i18n-download-translations.groovy
@@ -38,6 +38,8 @@ def runScript() {
 
          if (!overrideLangs) {
              dir("webapp") {
+                // This is in requirements.txt but is somehow not installed.
+                sh("pip install tabulate==0.7.5");
                 // If not passed in as a param, get the single highest priority 
                 // lang
                 overrideLangs = exec.outputOf(["deploy/order_download_i18n.py", 
