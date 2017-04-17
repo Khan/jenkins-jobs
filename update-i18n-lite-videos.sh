46569a39798f24139d5f5353e28af6a0303356a2
Craig Silverstein <csilvers@khanacademy.org>
2017-04-17 15:38:16 -0700

    Oops, I need to redo all the build.lib commands now I'm in a new directory.
    I'm now running the build.lib stuff from workspace-root rather than
    from webapp-root, so all the arguments change.
    
    This looks pretty ugly; it would be better to find another way of
    doing it.  But it may be that the right solution is to move the
    jenkins logic (and maybe this whole script!) into groovy.
    
    Auditors: james
    
    Test plan:
    Fingers crossed

--- update-i18n-lite-videos.sh
+++ update-i18n-lite-videos.sh
@@ -8,11 +8,11 @@
 
 echo "Updating the webapp repo."
 # We do our work in the 'translations' branch.
-jenkins-tools/build.lib safe_pull_in_branch . translations
+jenkins-tools/build.lib safe_pull_in_branch webapp translations
 # ...which we want to make sure is up-to-date with master.
-jenkins-tools/build.lib safe_merge_from_master . translations
+jenkins-tools/build.lib safe_merge_from_master webapp translations
 # We also make sure the intl/translations sub-repo is up to date.
-jenkins-tools/build.lib safe_pull intl/translations
+jenkins-tools/build.lib safe_pull webapp/intl/translations
 
 (
     cd webapp
@@ -26,6 +26,6 @@ jenkins-tools/build.lib safe_pull intl/translations
 export FORCE_COMMIT=1
 
 echo "Checking in new video-lists."
-jenkins-tools/build.lib safe_commit_and_push intl/translations \
+jenkins-tools/build.lib safe_commit_and_push webapp/intl/translations \
    -m "Automatic update of video_*.json" \
    -m "(at webapp commit `cd webapp && git rev-parse HEAD`)"
