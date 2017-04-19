bb98612a1fe38bbc5d176827369784877fa3adcd
Craig Silverstein <csilvers@khanacademy.org>
2017-04-18 17:21:57 -0700

    Move virtualenv-handling out of build.lib and into groovy.
    Summary:
    This is part of my effort to get rid of build.lib. Now the existing
    withVirtualenv.groovy does all the virtualenv work, rather than
    calling out to build.lib!
    
    This let me get rid of several global variables used in build.lib, which makes the script easier to reason about.  While in the area, I got rid of an unrelated global var, `MAKE`, which we never overrode anywhere.
    
    Test Plan: groovy vars/withVirtualenv.groovy
    
    Reviewers: amos, colin
    
    Reviewed By: amos
    
    Differential Revision: https://phabricator.khanacademy.org/D35174

--- vars/withVirtualenv.groovy
+++ vars/withVirtualenv.groovy
@@ -1,11 +1,45 @@
-// This must be called from a directory that has jenkins-tools checked
-// out as a subdir.
+//import vars.exec
+
+// This must be called from workspace-root.
 def call(Closure body) {
-    // `ensure_virtualenv` creates an `env` subdir with the virtualenv in it.
-    sh("./jenkins-tools/build.lib ensure_virtualenv");
-    def virtualenvDir = sh(script: "cd env && pwd", returnStdout: true).trim();
-    withEnv(["VIRTUAL_ENV=${virtualenvDir}",
-             "PATH=${virtualenvDir}/bin:${env.PATH}"]) {
-       body();
-    }
+   if (env.VIRTUAL_ENV) {    // we're already in a virtualenv
+      body();
+      return;
+   }
+   if (!fileExists("env")) {
+      echo("Creating new virtualenv(s)");
+
+      // We create a "normal" virtualenv we use most of the time, and
+      // a "dbg" virtualenv that uses python2.7-dbg and lets us debug
+      // running python processes using gdb.
+      sh("virtualenv --python=python env.normal");
+      if (exec.statusOf(["which", "python2.7-dbg"]) == 0) {
+         sh("virtualenv --python=python2.7-dbg env.dbg");
+         // Need one more fix, as per
+         // http://stackoverflow.com/questions/22931774/how-to-use-gdb-python-debugging-extension-inside-virtualenv
+         sh("cp -a /usr/lib/debug/usr/bin/python*gdb.py env.dbg/bin/");
+      }
+      // Have 'env' point to 'env.normal'.  To debug, you just manually
+      // change the symlink to point to env.dbg
+      sh("ln -snf env.normal env");
+
+      writeFile(file: "README.debugging",
+                text: """\
+If you want to be able to debug a running python process using gdb
+(to debug hangs or segfaults, say), do the following:
+    ln -snf env.dbg env
+    <run your python process>
+    gdb -p <python process id, from 'ps' or similar>
+    (gdb) py-bt    # etc
+For more information, see https://wiki.python.org/moin/DebuggingWithGdb
+""");
+   }
+
+   // There's no point in calling activate directly since we are not
+   // a shell.  Instead, we just set up the environment the same way
+   // activate does.
+   withEnv(["VIRTUAL_ENV=${pwd()}/env",
+            "PATH=${pwd()}/env/bin:${env.PATH}"]) {
+      body();
+   }
 }
