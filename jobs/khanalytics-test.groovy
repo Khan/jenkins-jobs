f1a47675de9f023526673bf0e75b0f6aeb377c5c
Colin Fuller <colin@khanacademy.org>
2017-04-28 09:16:22 -0400

    Jenkins job for khanalytics tests
    Summary:
    This commit sets up a simple job, modeled after webapp-test, for running the
    khanalytics lint, flow checker, and unit tests.  At the moment, I have
    installing the python deps and running the python unit tests disabled (with a
    TODO to reenable) because I still need to figure out how to get python 3.6 and
    a virtualenv running.  Lint also isn't working because it's trying to lint
    python 2.  But before I muck around with that, I want to make sure
    I've got the basics of a job working correctly.
    
    Beyond the python version, there's also a ton of TODOs because the makefile
    rules implicitly require docker, so we can't use them on jenkins.  In theory
    they don't need to, but it's complicated to get that to work; for now I'm just
    running the commands directly.  It's also still a TODO to do any sort of
    reporting beyond success/failure.
    
    Test Plan:
    - `groovy khanalytics-test.groovy`, see that the only errors are missing
      imports
    - create a job on jenkins and try it!
    - see a success message in #future-of-pipelines
    
    Reviewers: amos
    
    Reviewed By: amos
    
    Subscribers: tom, ragini, csilvers, #data-engineering
    
    Differential Revision: https://phabricator.khanacademy.org/D35403

---
+++ jobs/khanalytics-test.groovy
@@ -0,0 +1,89 @@
+// Pipeline job for khanalytics tests.
+//
+// This runs `make lint`, `make flow`, and `make check` on the khanalytics
+// repo.
+//
+// This does not run the integration tests `make allcheck` since that requires
+// docker and root access.  TODO(colin): figure out how to run these.
+
+@Library("kautils")
+
+import org.khanacademy.Setup;
+// Vars we use, under jenkins-tools/vars/.  This is just for documentation.
+//import vars.kaGit
+//import vars.notify
+//import vars.onMaster
+
+new Setup(steps
+
+).addStringParam(
+    "GIT_REVISION",
+    """The git commit-hash to run tests at, or a symbolic name referring
+    to such a commit-hash.""",
+    "master"
+
+).addStringParam(
+   "SLACK_CHANNEL",
+   "The slack channel to which to send failure alerts.",
+   "#future-of-pipelines"
+).apply();
+
+REPOSITORY = "git@github.com:Khan/khanalytics";
+
+// We set these to real values first thing below; but we do it within
+// the notify() so if there's an error setting them we notify on slack.
+GIT_SHA1 = null;
+
+def initializeGlobals() {
+    GIT_SHA1 = kaGit.resolveCommitish(REPOSITORY, params.GIT_REVISION);
+}
+
+def _setupKhanalytics() {
+    // TODO(colin): something in this script is trying to use secrets.py from
+    // the webapp dir, so we have to clone this.  Figure out what and why so we
+    // don't need to do this.
+    kaGit.safeSyncTo("git@github.com:Khan/webapp", "master");
+    kaGit.safeSyncTo("git@github.com:Khan/khan-linter", "master");
+    kaGit.safeSyncTo(REPOSITORY, GIT_SHA1);
+    dir("khanalytics") {
+        // TODO(colin): enable deps after figuring out python3 +
+        // virtualenv.
+        // TODO(colin): this should be `make deps`, but can't be because of an
+        // implicit dependency of the makefile on docker.
+        // sh("pip install -r ./requirements_dev.txt")
+        dir("core/monitor/src") {
+            // TODO(colin): move this into `make deps`?
+            sh("npm install");
+        }
+    }
+}
+
+def runTests() {
+    onMaster('10m') {
+        _setupKhanalytics();
+        dir("khanalytics") {
+            // TODO(colin): this should be `make lint`, but can't be because of
+            // an implicit dependency of the makefile on docker.
+            sh("../khan-linter/bin/ka-lint --blacklist=yes");
+            // TODO(colin): this should make `make flow`, but can't be because
+            // of an implicit dependency of the makefile on docker.
+            dir("core/monitor/src") {
+                sh("./node_modules/.bin/flow check .");
+            }
+            // TODO(colin): enable tests after figuring out python3 +
+            // virtualenv.
+            // TODO(colin): this should make `make check`, but can't be because
+            // of an implicit dependency of the makefile on docker.
+            // sh("./runtests.py");
+            // TODO(colin): some kind of summary report?
+        };
+    }
+}
+
+notify([slack: [channel: params.SLACK_CHANNEL,
+                sender: 'Testing Turtle',
+                emoji: ':turtle:',
+                when: ['FAILURE', 'UNSTABLE', 'ABORTED', 'SUCCESS']]]) {
+    initializeGlobals();
+    runTests();
+}
