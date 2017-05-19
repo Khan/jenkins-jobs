2561905c69671c305e931baddeb065e42ae0a24f
Colin Fuller <colin@khanacademy.org>
2017-05-19 11:51:23 -0400

    Add a jenkins job to send the recent deploy history to slack
    Summary:
    See https://app.asana.com/0/31965416896056/344186384206728
    
    Sometimes when we're tracking down a new error or problem, it's useful
    to be able to quickly find everything that was deployed to production
    recently.  This commit adds a jenkins job to send this information to
    slack (which will eventually be run from a sun command).
    
    Rather than wrestling with the jenkins API to find the last successful
    deploy-webapp jobs, I instead opted to exploit the fact that we create
    git tags for successful deploys when they're merged to master.  This
    allows us to find the last 5 deploys by grabbing the most recent tags
    (excluding any that were rolled back and marked with the `-bad` suffix).
    
    To print the changelog, I reused the function for doing that that exists
    in the webapp deploy package.
    
    The only thing this strategy doesn't get us vs the actual jenkins jobs
    is the name of the actual deployer, but I don't think this is a big deal
    since we do print the names of all the committers, so there's always
    someone to go contact.
    
    Test Plan:
    - create a pipeline job `deploy-history` on jenkins that pulls from a
      branch of jenkins-tools with this commit
    - run the job (reporting to #bot-testing)
    - verify it prints the five most recent deploys and their changelogs
    
    Reviewers: csilvers, amos
    
    Reviewed By: csilvers
    
    Subscribers: tom
    
    Differential Revision: https://phabricator.khanacademy.org/D35889

---
+++ jobs/deploy-history.groovy
@@ -0,0 +1,92 @@
+// Job to figure out the history of the last five webapp deploys
+// This lets us quickly print (via a sun command) commits that were deployed
+// recently to assist in debugging new issues.
+
+@Library("kautils")
+
+import org.khanacademy.Setup;
+// Vars we use, under jenkins-tools/vars.  This is just for documentation.
+//import vars.exec
+//import vars.kaGit
+//import vars.notify
+//import vars.onMaster
+
+new Setup(steps
+
+).addStringParam(
+    "SLACK_CHANNEL",
+    "The slack channel to which to sent the report.",
+    "#1s-and-0s-deploys"
+).apply();
+
+REPOSITORY = "git@github.com:Khan/webapp";
+CHAT_SENDER = "History Hawk";
+EMOJI = ":hawk";
+
+def doSetup() {
+    onMaster('30m') {
+        kaGit.safeSyncToOrigin(REPOSITORY, "master");
+    }
+}
+
+def _findMostRecentTags() {
+    return exec.outputOf(['deploy/git_tags.py']).split()[-6..-1].reverse();
+}
+
+def _sendChangelog(def startCommitish, def endCommitish) {
+    def start = kaGit.resolveCommitish(REPOSITORY, startCommitish);
+    def end = kaGit.resolveCommitish(REPOSITORY, endCommitish);
+    def pythonCommand = [
+        "deploy/chat_messaging.py",
+        "--chat-sender", CHAT_SENDER,
+        "--icon-emoji", EMOJI,
+        "--slack-channel", params.SLACK_CHANNEL,
+        start,
+        end
+    ]
+    // Secrets required to talk to slack.
+    withSecrets() {
+        dir('webapp') {
+            exec(pythonCommand);
+        }
+    }
+}
+
+def _sendSimpleMessage(def msg) {
+    def args = ["jenkins-tools/alertlib/alert.py",
+                "--slack=${params.SLACK_CHANNEL}",
+                "--chat-sender=${CHAT_SENDER}",
+                "--icon-emoji=${EMOJI}",
+                "--slack-simple-message"];
+    // Secrets required to talk to slack.
+    withSecrets() {
+        sh("echo ${exec.shellEscape(msg)} | ${exec.shellEscapeList(args)}");
+    }
+}
+
+def reportHistory() {
+    onMaster('5m') {
+        dir('webapp') {
+            tags = _findMostRecentTags();
+        }
+        _sendSimpleMessage("Here's the five most recent successful deploys (newest first):");
+        for (def i = 0; i < tags.size() - 1; i++) {
+            def start = tags[i + 1];
+            def end = tags[i];
+            _sendSimpleMessage("*${end}*");
+            _sendChangelog(start, end);
+        }
+    }
+}
+
+notify([slack: [channel: params.SLACK_CHANNEL,
+                sender: CHAT_SENDER,
+                emoji: EMOJI,
+                when: ['FAILURE', 'UNSTABLE', 'ABORTED']]]) {
+    stage("setup") {
+        doSetup();
+    }
+    stage("changelog") {
+        reportHistory();
+    }
+}
