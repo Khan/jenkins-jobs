78ede5c02bdb2f8fe006738fbd9f47339929b773
Craig Silverstein <csilvers@khanacademy.org>
2017-06-13 13:44:46 -0700

    Don't decrypt secrets when it's not necessary.
    Summary:
    We're seeing a steady trickle of "secrets.py is newer than
    secrets.py.cast5."  I //think// this happens when multiple jobs
    decrypt at the same time.
    
    One way to help with that is to not decrypt as often.
    secrets.py.cast5 changes very rarely, so we shouldn't be decrypting
    all the time.  We were before, but after this change we won't be.
    
    A real locking solution would be better, or perhaps just having the
    secrets-dir be per-job rather than global.  That's a TODO for now.
    
    Test Plan: Fingers crossed
    
    Reviewers: amos, colin
    
    Differential Revision: https://phabricator.khanacademy.org/D36514

--- vars/withSecrets.groovy
+++ vars/withSecrets.groovy
@@ -7,13 +7,30 @@ def secretsDir() {
 
 // This must be called from workspace-root.
 def call(Closure body) {
-   exec(["cp", "webapp/secrets.py.cast5", secretsDir()]);
-   dir(secretsDir()) {
-      // This decryption command was copied from the make target
-      // "secrets_decrypt" in the webapp project.
-      sh("openssl cast5-cbc -d -in secrets.py.cast5 -out secrets.py " +
-         "-kfile secrets.py.cast5.password");
-      sh("chmod 600 secrets.py");
+   // We enforce, here, the invariant that, inside secretsDir, the
+   // current secrets.py is always the decrypted version of the
+   // current secrets.py.cast5 if it exists.  If we can't enforce that
+   // we delete secrets.py.cast5 (thus trivially restoring the
+   // invariant).  That way, we know that if secrets.py.cast5 hasn't
+   // changed since the last call, there's nothing we need to do.
+   // TODO(csilvers): there's a race condition here if multiple jobs
+   // (or threads within a job) call this at the same time.
+   rc = exec.statusOf(["cmp", "webapp/secrets.py.cast5",
+                       "${secretsDir()}/secrets.py.cast5"]);
+   if (rc != 0) {   // means there are new secrets we need to decrypt
+      try {
+         exec(["cp", "webapp/secrets.py.cast5", secretsDir()]);
+         dir(secretsDir()) {
+            // This decryption command was copied from the make target
+            // "secrets_decrypt" in the webapp project.
+            sh("openssl cast5-cbc -d -in secrets.py.cast5 -out secrets.py " +
+               "-kfile secrets.py.cast5.password");
+            sh("chmod 600 secrets.py");
+         }
+      } catch (e) {
+         echo("Error decrypting secrets: ${e}.  Deleting the cast5 file.");
+         exec(["rm", "-f", "${secretsDir()}/secrets.py.cast5"]);
+      }
    }
    withEnv(["PYTHONPATH=${secretsDir()}:${env.PYTHONPATH}"]) {
       body();
