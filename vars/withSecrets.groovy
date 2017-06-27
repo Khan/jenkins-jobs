440ccd0a88d5dce5c6f61298a7242cf2a2d634b7
Ben Kraft <benkraft@khanacademy.org>
2017-06-27 14:45:28 -0700

    Another attempt at fixing globals
    That didn't work!  Hopefully this does, per
    https://stackoverflow.com/a/6315609.  It passes `groovy` locally, at least.
    Also, `groovy` locally complains that `env` isn't defined, so I put that back
    in a function.
    
    Test plan: `groovy vars/withSecrets.groovy`; deploy it
    
    Auditors: csilvers

--- vars/withSecrets.groovy
+++ vars/withSecrets.groovy
@@ -1,19 +1,18 @@
+import groovy.transform.Field
 //import vars.exec
 
-// For reasons I (benkraft) don't understand, we have to wrap globals in a
-// class; see https://issues.apache.org/jira/browse/GROOVY-546.
-class Globals {
-    // This is no longer where we store secrets.py, it's just where the
-    // password lives.
-    _SECRETS_PASSWORD_DIR = "${env.HOME}/secrets_py";
+// The number of active withSecrets blocks.  We only want to clean secrets up
+// at the end if we are exiting the last withSecrets block, since they likely
+// all share a workspace.
+// TODO(benkraft): In principle this should be per-directory; in practice
+// assuming we're always in the same directory is good enough at present.
+// TODO(benkraft): Make sure updates to this are actually atomic.
+@Field _activeSecretsBlocks = 0;
 
-    // The number of active withSecrets blocks.  We only want to clean secrets up
-    // at the end if we are exiting the last withSecrets block, since they likely
-    // all share a workspace.
-    // TODO(benkraft): In principle this should be per-directory; in practice
-    // assuming we're always in the same directory is good enough at present.
-    // TODO(benkraft): Make sure updates to this are actually atomic.
-    _activeSecretsBlocks = 0;
+// This is no longer where we store secrets.py, it's just where the
+// password lives.
+def _secretsPasswordDir() {
+   return "${env.HOME}/secrets_py";
 }
 
 
@@ -38,21 +37,20 @@ def call(Closure body) {
       exec(["openssl", "cast5-cbc", "-d",
             "-in", "${webappSecretsDir}/secrets.py.cast5",
             "-out", "${webappSecretsDir}/secrets.py",
-            "-kfile",
-            "${Globals._SECRETS_PASSWORD_DIR}/secrets.py.cast5.password"]);
+            "-kfile", "${_secretsPasswordDir()}/secrets.py.cast5.password"]);
       sh("chmod 600 ${webappSecretsDir}/secrets.py");
-      Globals._activeSecretsBlocks++;
+      _activeSecretsBlocks++;
 
       // Then, tell alertlib where secrets live, and run the wrapped block.
       withEnv(["ALERTLIB_SECRETS_DIR=${webappSecretsDir}"]){
          body();
       }
    } finally {
-      Globals._activeSecretsBlocks--;
+      _activeSecretsBlocks--;
       // Finally, iff we're exiting the last withSecrets block, clean up
       // secrets.py so if the next job intends to run without secrets, it does.
       // We remove both versions just in case an old one was floating around.
-      if (!Globals.activeSecretsBlocks) {
+      if (!activeSecretsBlocks) {
          sh("rm -f webapp/shared/secrets.py");
          sh("rm -f webapp/secrets.py");
       }
