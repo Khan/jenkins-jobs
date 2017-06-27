b063ea4f97afc03ad35e33834a3c03311c25ed68
Ben Kraft <benkraft@khanacademy.org>
2017-06-27 13:37:33 -0700

    Un-Revert "Change how jenkins handles secrets, and update for the new location of secrets"
    This is clearly going to need some more fixes, but we need to un-revert to test
    it on Jenkins.  This reverts 5d7037c6, 8f972a4d, and a95c487b, un-reverting
    0dc67d19 and its follow-ups e86a882e and 6f1998d5.
    
    Test plan: deploy with it
    
    Auditors: csilvers

--- vars/withSecrets.groovy
+++ vars/withSecrets.groovy
@@ -1,39 +1,43 @@
 //import vars.exec
 
-def secretsDir() {
+// This is no longer where we store secrets.py, it's just where the
+// password lives.
+def _secretsPasswordDir() {
    return "${env.HOME}/secrets_py";
 }
 
 
 // This must be called from workspace-root.
 def call(Closure body) {
-   // We enforce, here, the invariant that, inside secretsDir, the
-   // current secrets.py is always the decrypted version of the
-   // current secrets.py.cast5 if it exists.  If we can't enforce that
-   // we delete secrets.py.cast5 (thus trivially restoring the
-   // invariant).  That way, we know that if secrets.py.cast5 hasn't
-   // changed since the last call, there's nothing we need to do.
-   // TODO(csilvers): there's a race condition here if multiple jobs
-   // (or threads within a job) call this at the same time.
-   rc = exec.statusOf(["cmp", "webapp/secrets.py.cast5",
-                       "${secretsDir()}/secrets.py.cast5"]);
-   if (rc != 0) {   // means there are new secrets we need to decrypt
-      try {
-         exec(["cp", "webapp/secrets.py.cast5", secretsDir()]);
-         dir(secretsDir()) {
-            // This decryption command was copied from the make target
-            // "secrets_decrypt" in the webapp project.
-            sh("openssl cast5-cbc -d -in secrets.py.cast5 -out secrets.py " +
-               "-kfile secrets.py.cast5.password");
-            sh("chmod 600 secrets.py");
-         }
-      } catch (e) {
-         echo("Error decrypting secrets: ${e}.  Deleting the cast5 file.");
-         exec(["rm", "-f", "${secretsDir()}/secrets.py.cast5"]);
+   try {
+      // If this runs before the move of secrets.py is deployed,
+      // we need to operate on the old secrets file.
+      // TODO(benkraft): remove after the move is deployed, by
+      // 15 June 2017.
+      if (fileExists("webapp/shared/secrets.py.cast5")) {
+         webappSecretsDir = "webapp/shared";
+      } else {
+         webappSecretsDir = "webapp";
       }
-   }
-   withEnv(["PYTHONPATH=${secretsDir()}:${env.PYTHONPATH}"]) {
-      body();
+      // First, set up secrets.
+      // This decryption command was modified from the make target
+      // "secrets_decrypt" in the webapp project.
+      exec(["openssl", "cast5-cbc", "-d",
+            "-in", "${webappSecretsDir}/secrets.py.cast5",
+            "-out", "${webappSecretsDir}/secrets.py",
+            "-kfile", "${_secretsPasswordDir()}/secrets.py.cast5.password"]);
+      sh("chmod 600 ${webappSecretsDir}/secrets.py");
+
+      // Then, tell alertlib where secrets live, and run the wrapped block.
+      withEnv(["ALERTLIB_SECRETS_DIR=${webappSecretsDir}"]){
+         body();
+      }
+   } finally {
+      // Finally, clean up secrets.py so if the next job intends
+      // to run without secrets, it does.  We remove both versions
+      // just in case an old one was floating around.
+      sh("rm -f webapp/shared/secrets.py");
+      sh("rm -f webapp/secrets.py");
    }
 }
 
@@ -42,7 +46,7 @@ def call(Closure body) {
 // and secrets are present.
 // Use cautiously! -- this may not decrypt secrets for you.
 def ifAvailable(Closure body) {
-   if (fileExists("webapp/secrets.py.cast5")) {
+   if (fileExists("webapp/shared/secrets.py.cast5")) {
       call(body);
    }
 }
