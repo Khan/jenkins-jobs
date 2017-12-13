9018b6ab40647c4c45a1d2892b992c580e76968a
Sergei Orlov <sergei@khanacademy.org>
2017-12-13 15:56:31 -0800

    Pass buildmaster token in request
    Test Plan:
    <see below>

--- vars/buildmaster.groovy
+++ vars/buildmaster.groovy
@@ -3,12 +3,17 @@
 import groovy.json.JsonBuilder;
 
 
+def BUILDMASTER_TOKEN = readFile(
+   "${env.HOME}/buildmaster-api-token.secret").trim();
+
 // Make an API request to the buildmaster
 // `params` is expected to be a map
 def _makeHttpRequest(resource, httpMode, params) {
     def response = httpRequest(
         acceptType: "APPLICATION_JSON",
         contentType: "APPLICATION_JSON",
+        customHeaders: [[name: 'X-Buildmaster-Token',
+                         value: BUILDMASTER_TOKEN]],
         httpMode: httpMode,
         requestBody: JsonBuilder(params).toString(),
         url: "https://buildmaster.khanacademy.org/${resource}");
