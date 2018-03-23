8a8cfc7939b44d27b3f7e38db3f52a74597002ae
Ben Kraft <benkraft@khanacademy.org>
2018-03-23 11:18:36 -0700

    Fix a comment typo
    Test plan: install a spell checker
    
    Auditors: csilvers

--- jobs/deploy-webapp.groovy
+++ jobs/deploy-webapp.groovy
@@ -377,7 +377,7 @@ def promptForSetDefault() {
       // Send the changelog!
       withSecrets() {
          dir("webapp") {
-            // Prints the diff ROLLBACK_DO..GIT_REVISION (i.e. changes since
+            // Prints the diff ROLLBACK_TO..GIT_REVISION (i.e. changes since
             // the currently live version).
             exec(["deploy/chat_messaging.py", ROLLBACK_TO, GIT_REVISION,
                   // We omit the deployer username; the next message has an
