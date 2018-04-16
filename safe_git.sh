f8120cbc232b0a364f65beca91c71188345efeab
Craig Silverstein <csilvers@khanacademy.org>
2018-04-16 14:58:14 -0700

    Use the `-b` flag to `git clone` to save a bit of time.
    Auditors: benkraft
    
    Test plan:
    Fingers crossed

--- safe_git.sh
+++ safe_git.sh
@@ -115,6 +115,9 @@ _destructive_checkout() {
     | while read f; do
         if [ -d "$f" ]; then
             ( cd "$f" && _destructive_checkout HEAD )
+        else
+            # This "shouldn't" happen, but seems to in git-bigfile contexts.
+            git checkout -- "$f"
         fi
     done
 
@@ -171,14 +174,13 @@ clone() {
         # Now clone locally as well.
         # TODO(csilvers): figure out how to use `git clone -s` but still
         # have our version point to the correct remote.
-        timeout 60m git clone "$repo" "$repo_workspace"
+        timeout 60m git clone -b "$commit" "$repo" "$repo_workspace"
 
         cd "$repo_workspace"
         # This is the magic that makes all our repos share an "objects" dir.
         echo "$repo_dir/.git/objects" > .git/objects/info/alternates
         # Force our new repo to share the objects too.
         timeout 60m git gc
-        timeout 10m git checkout -f "$commit"
     fi
     )
 }
