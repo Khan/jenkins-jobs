905af680526fecb774e4055beffc42fbc57baef7
Ben Kraft <benkraft@khanacademy.org>
2018-07-02 13:05:34 -0700

    Allow deleting specific modules in delete-version
    Summary:
    This is in parallel to D46128 and must be deployed after it; it exposes
    the same option to Jenkins callers.
    
    Test Plan: deploy it, keep an eye on the version reaper
    
    Reviewers: csilvers, colin
    
    Reviewed By: csilvers
    
    Subscribers: #devops
    
    Differential Revision: https://phabricator.khanacademy.org/D46129

--- jobs/delete-version.groovy
+++ jobs/delete-version.groovy
@@ -19,6 +19,12 @@ spaces. Must exactly match the name of an existing version (e.g.
 "znd-170101-sean-znd-test 170101-1200-deadbeef1234")""",
     ""
 
+).addStringParam(
+    "MODULES",
+    """The modules to delete, comma-separated.  By default we delete all the
+"webapp" modules.""",
+    ""
+
 ).apply();
 
 currentBuild.displayName = "${currentBuild.displayName} (${params.VERSION})";
@@ -36,10 +42,14 @@ def deleteVersion() {
       kaGit.safeSyncToOrigin("git@github.com:Khan/webapp", "master");
       dir("webapp") {
          sh("make python_deps");
-         exec(["deploy/delete_gae_versions.py"]
-              // We need to cast because split() returns an Array and groovy
-              // wants a List, apparently.
-              + (params.VERSION.split(" ") as List));
+         def args = (["deploy/delete_gae_versions.py"]
+                     // We need to cast because split() returns an Array and
+                     // groovy wants a List, apparently.
+                     + (params.VERSION.split(" ") as List));
+         if (params.MODULES) {
+            args += ["--modules", params.MODULES];
+         }
+         exec(args);
       }
    }
 }
