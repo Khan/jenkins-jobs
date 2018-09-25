dfc7f1587c183a51d7625756ec765a92319853c3
Neha <nehabagdia@C02S68D0G8WN.local>
2018-09-25 10:50:12 -0700

    Adding new files for the new jenkins job.

---
+++ create-all-pot-pickle.sh
@@ -0,0 +1,69 @@
+#!/bin/bash -xe
+
+# This script is run by the jenkins 'i18n-create-all-pot-pickle' job
+
+( cd webapp && make install_deps )
+
+DATA_DIR=`readlink -f /mnt/webapp-i18n-data`
+
+echo "Updating the webapp repo."
+# We do our work in the 'automated-commits' branch.
+jenkins-jobs/safe_git.sh pull_in_branch webapp automated-commits
+# ...which we want to make sure is up-to-date with master.
+jenkins-jobs/safe_git.sh merge_from_master webapp automated-commits
+# We also make sure the intl/translations sub-repo is up to date.
+jenkins-jobs/safe_git.sh pull webapp/intl/translations
+
+cd webapp
+
+echo "Starting update_strings job at $(date +%H:%M:%S)"
+
+echo "Downloading and extracting sync snapshot"
+# find_graphie_images_in_items.js needs this snapshot of article content in
+# order to extract images from articles.
+echo "Starting extracting sync snapshot at $(date +%H%M)"
+gsutil cp gs://ka_dev_sync/snapshot_en snapshot_en
+tools/extract_lintable_content.py \
+    --articles \
+    --input snapshot_en \
+    --output article_content.zip
+echo "Ending extracting sync snapshot at $(date +%H:%M:%S)"
+
+echo "Updating the list of graphie images."
+# find_graphie_images_in_items.js caches items here, so we create the directory
+# for it.
+echo "Starting updating the list of graphie images at $(date +%H:%M:%S)"
+mkdir -p genfiles/assessment_items
+dev/tools/run_js_in_node.js content_editing/tools/find_graphie_images_in_items.js
+echo "Ending updating the list of graphie images at $(date +%H%M)"
+
+echo "Creating a new, up-to-date all.pot."
+echo "Starting to create a new, up-to-date all.pot at $(date +%H:%M:%S)"
+# Both handlebars.babel and shared_jinja.babel look for popular_urls in /tmp,
+# but we also want to keep a version in source control for debugging purposes.
+# TODO(csilvers): uncomment once we get popular_pages up and using bigquery.
+#tools/popular_pages.py --limit 10000 > "$DATA_DIR"/popular_urls
+cp -f "$DATA_DIR"/popular_urls /tmp/
+# By removing genfiles/extracted_strings/en/intl/datastore.pot.pickle,
+# we force compile_all_pot to re-fetch nltext datastore info from prod.
+rm -f genfiles/extracted_strings/en/intl/datastore.pot.pickle
+echo "Starting build_prod_main $(date +%H:%M:%S)"
+build/kake/build_prod_main.py -v3 new_pot
+# This is where build_prod_main.py puts the output all.pot
+echo "Ending build_prod_main $(date +%H:%M:%S)"
+ALL_POT="$PWD"/genfiles/translations/new/all.pot.pickle
+
+echo "Sanity check: will fail if the new all.pot is missing stuff."
+[ `strings "$ALL_POT" | wc -l` -gt 3000000 ]
+strings "$ALL_POT" | grep -q 'intl/datastore'
+
+# Update export timestamps for fake languages.
+mark_fake_langs=`cat <<PYCOMMAND
+from deploy import download_i18n
+download_i18n.mark_strings_export('accents')
+download_i18n.mark_strings_export('boxes')
+PYCOMMAND
+`
+python -c "$mark_fake_langs"
+
+cp "$ALL_POT" "$DATA_DIR"/all.pot
---
+++ jobs/i18n-create-all-pot-pickle.groovy
@@ -0,0 +1,40 @@
+// Pipeline job that does the following:
+// * Creates the all.pot.pickle file by collecting all strings that need
+//   translations. (calls create-all-pot-pickle.sh)
+
+
+@Library("kautils")
+// Classes we use, under jenkins-jobs/src/.
+import org.khanacademy.Setup;
+// Vars we use, under jenkins-jobs/vars/.  This is just for documentation.
+//import vars.exec
+//import vars.kaGit
+//import vars.notify
+//import vars.withTimeout
+//import vars.withSecrets
+
+
+new Setup(steps
+
+).addCronSchedule("H 2 * * *"
+
+).apply();
+
+
+def runScript() {
+   withTimeout('5h') {
+      kaGit.safeSyncToOrigin("git@github.com:Khan/webapp", "master");
+
+      // Remove output from a previous run.  Re-created by
+      // update-translations.
+      sh("rm -f updated_locales.txt")
+
+      // TODO(csilvers): see if we can break up this script into
+      // pieces, so we can put using-a-lot-of-memory only around
+      // the parts that use a lot of memory.
+
+      withSecrets() {
+          sh("jenkins-jobs/create-all-pot-pickle.sh")
+      }
+   }
+}
