36c0b99b334aa2e4312cfda22bf2795f3f0a541e
Kai Jing <kai@khanacademy.org>
2018-09-27 16:59:20 -0700

    enable test-worker task for try again

--- jobs/webapp-test.groovy
+++ jobs/webapp-test.groovy
@@ -423,5 +423,28 @@ def analyzeResults() {
 
 
 onWorker('ka-test-ec2', '5h') {     // timeout
+   notify([slack: [channel: params.SLACK_CHANNEL,
+                   thread: params.SLACK_THREAD,
+                   sender: 'Testing Turtle',
+                   emoji: ':turtle:',
+                   when: ['FAILURE', 'UNSTABLE']],
+           aggregator: [initiative: 'infrastructure',
+                        when: ['SUCCESS', 'BACK TO NORMAL',
+                               'FAILURE', 'ABORTED', 'UNSTABLE']],
+           buildmaster: [sha: params.GIT_REVISION,
+                         what: 'webapp-test']]) {
+      initializeGlobals();
 
+      try {
+         stage("Determining splits & running tests") {
+            determineSplitsAndRunTests();
+         }
+      } finally {
+         // We want to analyze results even if -- especially if --
+         // there were failures; hence we're in the `finally`.
+         stage("Analyzing results") {
+            analyzeResults();
+         }
+      }
+   }
 }
