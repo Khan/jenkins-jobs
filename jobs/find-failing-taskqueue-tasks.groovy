d8d84144c9115c87ce259e00dd4b3bcb6905d215
Craig Silverstein <csilvers@khanacademy.org>
2018-10-10 11:34:11 -0700

    Add a jenkins task to run the failing-taskqueue-tasks script once a week.
    Summary:
    I could have made this a cronjob on toby, but toby doesn't have webapp
    currently and I wasn't sure what gcloud permissions it had.  For
    jenkins, those things are already set up!  I'm not sure what the
    future of toby is anyway, so I didn't want to add more stuff to it.
    
    Test Plan:
    I ran `groovy jobs/find-failing-taskqueue-tasks.groovy` and got only
    the expected 2 "unable to resolve" errors.  We'll have to try running
    it! -- but it's a pretty simple script.
    
    Reviewers: benkraft, amos
    
    Reviewed By: benkraft
    
    Differential Revision: https://phabricator.khanacademy.org/D49172

---
+++ jobs/find-failing-taskqueue-tasks.groovy
@@ -0,0 +1,38 @@
+// A pipeline job to run the failing_taskqueue_tasks script.
+
+@Library("kautils")
+// Classes we use, under jenkins-jobs/src/.
+import org.khanacademy.Setup;
+//import vars.exec
+//import vars.kaGit
+//import vars.notify
+
+
+new Setup(steps
+
+).addStringParam(
+   "SLACK_CHANNEL",
+   "The slack channel to send our results, or empty string to disable.",
+   "#infrastructure"
+
+).addCronSchedule(
+   '0 7 * * 1'        // Run every monday morning at 7am
+
+).apply();
+
+
+onMaster('1h') {
+   notify([slack: [channel: params.SLACK_CHANNEL,
+                   sender: 'Taskqueue Totoro',
+                   emoji: ':totoro:',
+                   when: ['FAILURE', 'UNSTABLE', 'ABORTED']],
+           aggregator: [initiative: 'infrastructure',
+                        when: ['SUCCESS', 'BACK TO NORMAL',
+                               'FAILURE', 'ABORTED', 'UNSTABLE']]]) {
+      stage("Running script") {
+         kaGit.safeSyncToOrigin("git@github.com:Khan/webapp", "master");
+         exec(["webapp/dev/tools/failing_taskqueue_tasks.py",
+               "--slack-channel=${params.SLACK_CHANNEL}"]);
+      }
+   }
+}
