fff2486aef44962ea849de1b22570891646502ff
Colin Fuller <colin@khanacademy.org>
2018-10-22 10:41:31 -0400

    Revert "Deploying kotlin to appengine standard, part 2: jenkins deploy/set-default"
    This reverts commit 2854664c150bc868f946ab510395de0aa4875037.
    
    I had to update the jenkins workers with a new gcloud component in order for
    this to work. In the meantime, I'm temporarily reverting this commit to unblock
    the queue.
    
    Test plan: fingers crossed
    
    Auditors: benkraft, csilvers

--- jobs/deploy-webapp.groovy
+++ jobs/deploy-webapp.groovy
@@ -508,31 +508,51 @@ def verifySmokeTestResults(defaultSet, buildmasterFailures=0) {
 }
 
 
-def _promoteServices() {  // call from webapp-root
-    def cmd = ["deploy/set_default.py"];
+def _promoteWebapp() {  // call from webapp-root
+   if (!('static' in SERVICES || 'dynamic' in SERVICES)) {
+      return;
+   }
+
+   def cmd = ["deploy/set_default.py"];
+
+   if ('static' in SERVICES && !('dynamic' in SERVICES)) {
+      // TODO(benkraft): Have set_default.py just choose its own version
+      // against which to set-default in this case.
+      def gaeVersion = exec.outputOf(["deploy/current_version.py",
+                                      "--service", "dynamic"]);
+      cmd += [gaeVersion, "--static-content-version=${NEW_VERSION}"];
+   } else {
+      cmd += [NEW_VERSION];
+   }
 
     // If we're not deploying a new version of static content, the Sentry
     // version number should not be updated
-    if (!('static' in SERVICES)) {
-        cmd += ["--keep-error-version"];
-    }
+   if (!('static' in SERVICES)) {
+       cmd += ["--keep-error-version"]
+   }
 
-    cmd += ["--previous-tag-name=${ROLLBACK_TO}",
-             "--slack-channel=${SLACK_CHANNEL}",
-             "--deployer-username=${DEPLOYER_USERNAME}",
-             "--pretty-deployer-username=${PRETTY_DEPLOYER_USERNAME}"];
+   cmd += ["--previous-tag-name=${ROLLBACK_TO}",
+           "--slack-channel=${SLACK_CHANNEL}",
+           "--deployer-username=${DEPLOYER_USERNAME}",
+           "--pretty-deployer-username=${PRETTY_DEPLOYER_USERNAME}"];
 
-    if (params.SKIP_PRIMING) {
-        cmd += ["--no-priming"];
-    }
+   if (params.SKIP_PRIMING) {
+      cmd += ["--no-priming"];
+   }
 
-    def services = [];
-    for (def i = 0; i < SERVICES.size(); i++) {
-        services += ["${SERVICES[i]}=${NEW_VERSION}"]
-    }
+   exec(cmd);
+}
 
-    cmd += [services.join(",")];
-    exec(cmd);
+
+def _promoteKotlinRoutes() {  // call from webapp-root
+   if (!('kotlin-routes' in SERVICES)) {
+      return;
+   }
+
+   // We do the cd in the shell to avoid tmpdir issues -- see comments in
+   // build-webapp for details.
+   sh("cd services/kotlin_routes && ./gradlew set_default " +
+      "--new-version='${NEW_VERSION}'");
 }
 
 
@@ -540,7 +560,10 @@ def _promote() {
    withSecrets() {
       dir("webapp") {
          try {
-            _promoteServices();
+            parallel(
+               [ "promote-kotlin-routes": { _promoteKotlinRoutes(); },
+                 "promote-webapp": { _promoteWebapp(); },
+               ]);
 
             // Once we finish (successfully) promoting, we tell buildmaster
             // that the default has been set.  (Currently this information
