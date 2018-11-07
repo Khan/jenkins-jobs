628c01ac0f910913d0dd1d71a3327da1671d632d
Ben Kraft <benkraft@khanacademy.org>
2018-11-07 10:08:13 -0800

    Retry requests to the buildmaster from Jenkins
    The buildmaster sometimes flakes.  Now we'll retry!  For now, I just
    retry everything, because it was too hard to figure out how to detect
    whether the error is 4xx in a clean way.  Most of those 4xxes are pretty
    fast, anyway, so it's not a big cost.
    
    Test plan: cross fingers
    
    Reviewers: csilvers, jacqueline, kai

---
+++ vars/retry.groovy
@@ -0,0 +1,20 @@
+def call(options=null, Closure body) {
+   options = options ?: [:];
+   def retryCount = options.retryCount ?: 3;
+   for (def i = 0; i <= retryCount; i++) {
+      try {
+         body()
+      } catch (e) {
+         def msgStart = "Try #${i}: got ${e}."
+         if (i == retryCount) {
+            echo("${msgStart} Giving up.");
+            throw e;
+         } else if (options.rejectFn && options.rejectFn(e)) {
+            echo("${msgStart} Non-retriable error.");
+            throw e;
+         } else {
+            echo("${msgStart} Retrying.");
+         }
+      }
+   }
+}
