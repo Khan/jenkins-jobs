a76fb77d61c657e3d09cd8c72131b09c968eb871
Kai Jing <kai@khanacademy.org>
2019-03-20 11:44:32 -0700

    setup notify for testing

--- jobs/build-webapp.groovy
+++ jobs/build-webapp.groovy
@@ -510,7 +510,13 @@ def finishWithFailure(why) {
 // We use a build worker, because this is a very CPU-heavy job and we may want
 // to run several at a time.
 onWorker('build-worker', '4h') {
-   notify() {
+  notify([slack: [channel: '',
+                   sender: 'Mr Monkey',
+                   emoji: ':monkey_face:',
+                   // We don't need to notify on start because the buildmaster
+                   // does it for us; on success the we explicitly send
+                   // alertMsgs.SUCCESS.
+                   when: ['ABORTED']]) {
       stage("Merging in master") {
          mergeFromMasterAndInitializeGlobals();
       }
