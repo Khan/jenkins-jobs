0db8a846f677945c08774b3430ca52cd22e60566
Neha <nbagdia@gmail.com>
2019-04-16 16:09:41 -0700

    Rudimentary job to run locale specific information from jenkins.
    Summary:
    This will run all locales when scheduled and can also run on demand locale if needed.
    It will push TAP data into bigquery which will then be consumed by the Mode report Patrick created.
    
    Test Plan:
    Tests I ran:
    On dev box : ./run_translation_stats.sh ar
    On jenkins box: groovy run_translation_stats.groovy (This needs more changes like setting Params, setting up groovy, gradle and running environment setup)
    
    Reviewers: arunbakt, aasmund
    
    Reviewed By: arunbakt
    
    Subscribers: tom
    
    Differential Revision: https://phabricator.khanacademy.org/D53433

---
+++ jobs/run-translation-stats.groovy
@@ -0,0 +1,49 @@
+// Jenkins job that pushes
+// translation stats to bigquery and modeanalytics
+// for translators to keep track of progress
+
+@Library("kautils")
+import org.khanacademy.Setup;
+
+new Setup(steps
+
+).addCronSchedule("H/6 * * * *"
+
+).addStringParam(
+        "LOCALE",
+).apply();
+
+
+def runScript() {
+    def locale = params.LOCALE;
+
+    if (!locale) {
+        // Meaning this is a scheduled job that needs to run on all locales
+        // This could be copied from across AUTOMATICALLY_UPDATED_LOCALES
+        def list = ['bg', 'cs', 'de', 'es', 'fr', 'gu', 'hi', 'hy', 'id', 'ja', 'ka', 'ko',
+                    'nl', 'pt', 'pt-pt', 'sr', 'sv', 'ta', 'tr', 'zh-hans'];
+        for (item in list) {
+            println(item);
+            withSecrets() {
+                sh("jenkins-jobs/run_translation_stats.sh ${item}");
+            }
+        }
+    } else {
+        lock("using-a-lot-of-memory") {
+            withSecrets() {
+                sh("jenkins-jobs/run_translation_stats.sh ${locale}");
+            }
+        }
+    }
+}
+
+onMaster('1h') {
+    notify([slack: [channel: '#cp-eng',
+                    when: ['FAILURE', 'UNSTABLE']],
+            aggregator: [initiative: 'content-platform',
+                         when: ['FAILURE', 'UNSTABLE']]]) {
+        stage("Running script") {
+            runScript();
+        }
+    }
+}
