19c1c2ca0225f8883e77b0466cb4fa7d3bec93e8
Neha <nbagdia@gmail.com>
2019-04-16 16:02:33 -0700

    Feedback fixed.

--- jobs/translation-analytics.groovy
+++ jobs/translation-analytics.groovy
@@ -7,32 +7,25 @@ import org.khanacademy.Setup;
 // Vars we use, under jenkins-jobs/vars/.  This is just for documentation.
 //import vars.kaGit
 //import vars.notify
-//import vars.onWorker
 //import vars.withSecrets
 
 new Setup(steps
-
 ).addCronSchedule("H/1 * * * *"
-
-).addStringParam(
-        "LOCALE",
+).addStringParam("LOCALE",
 ).apply();
 
 
 def runScript() {
-    kaGit.safeSyncToOrigin("git@github.com:Khan/webapp",
-            params.GIT_REVISION);
+    kaGit.safeSyncToOrigin("git@github.com:Khan/webapp", params.GIT_REVISION);
 
     dir("webapp") {
         sh("make clean_pyc");    // in case some .py files went away
         sh("make deps");
     }
 
-    def locale = params.LOCALE;
-
     lock("using-a-lot-of-memory") {
         withSecrets() {
-            sh("jenkins-jobs/tap-setup.sh ${locale}");
+            sh("jenkins-jobs/tap-run.sh ${params.LOCALE}");
         }
     }
 }
