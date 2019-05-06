79509ea281cd79f6a8359a249d1a821816bcd30e
Tom W. Yedwab <tom@khanacademy.org>
2019-05-06 15:22:05 -0700

    Build combined_template_strings.json before running TAP job.
    Auditors: neha
    
    Test plan: Ran TAP locally.

--- tap-run.sh
+++ tap-run.sh
@@ -13,5 +13,10 @@ cd webapp
 make deps
 make current.sqlite
 
+# We need one file that is generated at build time. We can't run with
+# USE_PROD_FILES disabled as we prefer to get the most recent translation files
+# from GCS, so just build the one file manually to start with.
+build/kake/build_prod_main.py genfiles/combined_template_strings/combined_template_strings.json
 export USE_PROD_FILES=1
+
 tools/devshell.py --prod --log_level INFO --script tools/run_translation_analytics.py "$1"
