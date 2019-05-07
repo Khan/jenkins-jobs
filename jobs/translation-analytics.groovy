3e1dd21a5b93eaa738a12c1986401ec405b0a028
Neha <nbagdia@gmail.com>
2019-05-07 12:49:50 -0700

    Changes to parallelize the TAP runs.
    Test plan: Crossed fingers and praying

--- jobs/translation-analytics.groovy
+++ jobs/translation-analytics.groovy
@@ -20,24 +20,59 @@ def runScript() {
     kaGit.safeSyncToOrigin("git@github.com:Khan/webapp", "tap", [], true);
 
     dir("webapp") {
+        // remove any existing database
+        sh("rm -f current.sqlite");
         sh("make clean_pyc");    // in case some .py files went away
         sh("make deps");
+        sh("make current.sqlite");
+        sh("build/kake/build_prod_main.py genfiles/combined_template_strings/combined_template_strings.json");
     }
+}
 
-    lock("using-a-lot-of-memory") {
-        withSecrets() {
-            sh("jenkins-jobs/tap-run.sh ${params.LOCALE}");
-        }
+def runTapForLocaleOnly() {
+    withSecrets() {
+        sh("echo runTapForLocaleOnly ${params.LOCALE}");
+        sh("jenkins-jobs/tap-run.sh ${params.LOCALE}");
+    }
+}
+
+def runTapForLocaleAndEn() {
+    withSecrets() {
+        sh("echo runTapForLocaleAndEn ${params.LOCALE}");
+        sh("jenkins-jobs/tap-run.sh ${params.LOCALE} ${"en"} ${"false"}");
+    }
+}
+def runTapForLocaleAndStagedContent() {
+    withSecrets() {
+        sh("echo runTapForLocaleAndStagedContent ${params.LOCALE}");
+        sh("jenkins-jobs/tap-run.sh ${params.LOCALE} ${params.LOCALE} ${"true"}");
     }
 }
 
 onMaster('4h') {
-    notify([slack: [channel: '#cp-eng',
-                    when: ['FAILURE', 'UNSTABLE']],
+    notify([slack     : [channel: '#cp-eng',
+                         when   : ['FAILURE', 'UNSTABLE']],
             aggregator: [initiative: 'content-platform',
-                         when: ['FAILURE', 'UNSTABLE']]]) {
+                         when      : ['FAILURE', 'UNSTABLE']]]) {
         stage("Running script") {
             runScript();
         }
+        stage("Parallel processing the TAP runs") {
+            steps {
+                parallel(
+                        "firstTap": {
+                            runTapForLocaleOnly();
+                        },
+                        "secondTap": {
+                            runTapForLocaleAndEn();
+                        },
+                        "thirdTap": {
+                            runTapForLocaleAndStagedContent();
+                        }
+                )
+            }
+        }
     }
 }
+
+
--- tap-run.sh
+++ tap-run.sh
@@ -6,17 +6,12 @@
 #
 # For this script to work, secrets.py must be on the PYTHONPATH.
 
-# remove any existing database
-rm -f current.sqlite
-
 cd webapp
-make deps
-make current.sqlite
 
 # We need one file that is generated at build time. We can't run with
 # USE_PROD_FILES disabled as we prefer to get the most recent translation files
 # from GCS, so just build the one file manually to start with.
-build/kake/build_prod_main.py genfiles/combined_template_strings/combined_template_strings.json
+
 export USE_PROD_FILES=1
 
-tools/devshell.py --prod --log_level INFO --script tools/run_translation_analytics.py "$1"
+tools/devshell.py --prod --log_level INFO --script tools/run_translation_analytics.py --locale "$1" --content_locale "$2" --use_staged_content "$3"
