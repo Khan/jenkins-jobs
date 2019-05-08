a10b9746694d6b241b19edd44133c758c9e3444a
Neha <nbagdia@gmail.com>
2019-05-07 17:29:44 -0700

    Changes to parallelize the TAP runs.
    Test Plan: Crossed fingers and praying
    
    Reviewers: tom
    
    Reviewed By: tom
    
    Subscribers: tom
    
    Differential Revision: https://phabricator.khanacademy.org/D54178

--- jobs/translation-analytics.groovy
+++ jobs/translation-analytics.groovy
@@ -20,24 +20,62 @@ def runScript() {
     kaGit.safeSyncToOrigin("git@github.com:Khan/webapp", "tap", [], true);
 
     dir("webapp") {
+        // remove any existing database
+        sh("rm -f current.sqlite");
         sh("make clean_pyc");    // in case some .py files went away
         sh("make deps");
+        sh("make current.sqlite");
+        // We need one file that is generated at build time. We can't run with
+        // USE_PROD_FILES disabled as we prefer to get the most recent translation files
+        // from GCS, so just build the one file manually to start with.
+        sh("build/kake/build_prod_main.py genfiles/combined_template_strings/combined_template_strings.json");
     }
+}
 
-    lock("using-a-lot-of-memory") {
-        withSecrets() {
-            sh("jenkins-jobs/tap-run.sh ${params.LOCALE}");
-        }
+def runTapForLocaleOnly() {
+    withSecrets() {
+        sh("echo runTapForLocaleOnly ${params.LOCALE}");
+        sh("jenkins-jobs/tap-run.sh ${params.LOCALE} ${params.LOCALE} False");
+    }
+}
+
+def runTapForLocaleAndEn() {
+    withSecrets() {
+        sh("echo runTapForLocaleAndEn ${params.LOCALE} ");
+        sh("jenkins-jobs/tap-run.sh en ${params.LOCALE} False");
+    }
+}
+def runTapForLocaleAndStagedContent() {
+    withSecrets() {
+        sh("echo runTapForLocaleAndStagedContent ${params.LOCALE}");
+        sh("jenkins-jobs/tap-run.sh ${params.LOCALE} ${params.LOCALE} True");
     }
 }
 
 onMaster('4h') {
-    notify([slack: [channel: '#cp-eng',
-                    when: ['FAILURE', 'UNSTABLE']],
+    notify([slack     : [channel: '#cp-eng',
+                         when   : ['FAILURE', 'UNSTABLE']],
             aggregator: [initiative: 'content-platform',
-                         when: ['FAILURE', 'UNSTABLE']]]) {
+                         when      : ['FAILURE', 'UNSTABLE']]]) {
         stage("Running script") {
             runScript();
         }
+        stage("Parallel processing the TAP runs") {
+            steps {
+                parallel(
+                        "firstTap": {
+                            runTapForLocaleOnly();
+                        },
+                        "secondTap": {
+                            runTapForLocaleAndEn();
+                        },
+                        "thirdTap": {
+                            runTapForLocaleAndStagedContent();
+                        }
+                )
+            }
+        }
     }
 }
+
+
