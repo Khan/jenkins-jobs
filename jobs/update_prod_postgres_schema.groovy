93e59cf5c1636aa1ce5fb1cf27dfe6c4d01a0f00
Sean Driedger <sean@khanacademy.org>
2019-07-17 14:51:55 -0500

    Adding a Groovy job to run the Postgres migrations process
    Summary:
    Jira: https://khanacademy.atlassian.net/browse/DIST-480
    
    Postgres migrations can only be run from IP addresses explicitly whitelisted in
    the Cloud Console. Rather than whitelisting individual developer's IP addresses
    (and cleaning them up again afterwards), we'd like to simply whitelist the
    Jenkins IP address and have Jenkins run all migrations. (This is probably also
    the Right Thing To Do Long Term, since this brings postgres migrations one step
    closer to the rest of our Jenkins-driven deploy process.)
    
    This diff adds a Groovy job that will trigger the `migrate_prod` process, and
    report success (or failure) to #reports-eng.
    
    Test Plan: * Fingers crossed!
    
    Reviewers: benkraft, csilvers
    
    Reviewed By: csilvers
    
    Subscribers: dhruv
    
    Differential Revision: https://phabricator.khanacademy.org/D56235

---
+++ jobs/update_prod_postgres_schema.groovy
@@ -0,0 +1,40 @@
+// Update the production postgres db with the most recent migrations from
+// master
+
+@Library("kautils")
+// Classes we use, under jenkins-jobs/src/.
+import org.khanacademy.Setup;
+// Vars we use, under jenkins-jobs/vars/.  This is just for documentation.
+//import vars.kaGit
+//import vars.onMaster
+//import vars.notify
+//import vars.withTimeout
+//import vars.withSecrets
+
+
+// The simplest setup ever! -- we only want the defaults.
+new Setup(steps).apply();
+
+
+def runUpdate() {
+   withTimeout('1h') {
+      kaGit.safeSyncToOrigin("git@github.com:Khan/webapp", "master");
+      withSecrets() {      // we need postgres db secrets
+         dir("webapp/coaches/reports") {
+            sh("make migrate_prod")
+         }
+      }
+   }
+}
+
+
+onMaster('1h') {
+   notify([slack: [channel: '#reports-eng',
+                   sender: 'Migration Mouse',
+                   emoji: ':mouse:',
+                   when: ['SUCCESS', 'FAILURE', 'UNSTABLE', 'ABORTED']]]) {
+      stage("Running update") {
+         runUpdate();
+      }
+   }
+}
