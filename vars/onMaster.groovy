89b6d38b056f42cc3e62fae1d0b8284a0f3133e1
Ben Kraft <benkraft@khanacademy.org>
2019-07-24 18:02:05 -0700

    Add Go binaries to the path
    With Canonical's "exciting" new package manager comes a new location for
    binaries.  For some reason the workers aren't getting their path from
    /etc/profile like one would expect; comments suggest this is a known-ish
    issue for us.  So I added it to `onWorker`.
    
    Test plan: cross fingers, tests of my new Go code should start passing.
    
    Auditors: csilvers, kai

--- vars/onMaster.groovy
+++ vars/onMaster.groovy
@@ -11,6 +11,8 @@ def call(timeoutString, Closure body) {
             withTimeout(timeoutString) {
                 // To export BOTO_CONFIG, for some reason, master did not
                 // source the .profile or .bashrc anymore.
+                // TODO(benkraft): Should this also do the path-munging that
+                // onWorker does?
                 withEnv(["BOTO_CONFIG=${env.HOME}/.boto"]) {
                      body();
                 }
