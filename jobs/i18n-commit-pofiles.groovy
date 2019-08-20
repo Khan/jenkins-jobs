bea9e843bbbd4c2806ec9ae6af241a763aa2a84d
Tom W. Yedwab <tom@khanacademy.org>
2019-08-20 08:54:46 -0700

    New job: Commit pofiles to webapp. (From i18n-download-translations)
    Summary:
    The bulk of the work in the i18n-download-translations job is being moved off
    Jenkins and onto Kubernetes, however there's a fair bit of state and logic left
    here just around cloning webapp and then committing pofiles to the
    intl/translations subrepo. I'm going to leave this logic on Jenkins for the
    moment, and just fetch the pofiles from GCS as a way to intermediate the two
    parts of the job. Crowdin-go will invoke this job when the rest of the
    download-translations work has been completed.
    
    Test Plan: - Crossed fingers - will test on live Jenkins.
    
    Reviewers: neha
    
    Differential Revision: https://phabricator.khanacademy.org/D57262

---
+++ jobs/i18n-commit-pofiles.groovy
@@ -0,0 +1,69 @@
+// Pipeline job that commits pofiles to webapp.
+
+@Library("kautils")
+// Classes we use, under jenkins-jobs/src/.
+import org.khanacademy.Setup;
+// Vars we use, under jenkins-jobs/vars/.  This is just for documentation.
+//import vars.exec
+//import vars.notify
+//import vars.withTimeout
+//import vars.withSecrets
+
+
+new Setup(steps
+
+).addStringParam(
+    "LOCALE",
+    """Set this to the name of a locale to process, e.g., \"fr\".""",
+    ""
+
+).addStringParam(
+    "ARCHIVEID",
+    """The archive file to download from GCS (LOCALE-ARCHIVEID.pofiles.tar.gz).""",
+    ""
+
+).apply();
+
+
+def runScript() {
+   withTimeout('1h') {
+      kaGit.safeSyncToOrigin("git@github.com:Khan/webapp", "master");
+
+      dir("webapp") {
+         sh("make clean_pyc");
+         sh("make python_deps");
+      }
+
+      def locale = params.LOCALE;
+      def archiveid = params.ARCHIVEID;
+
+      currentBuild.displayName = ("${currentBuild.displayName} " +
+                                  "(${locale})");
+
+      withSecrets() {
+         withEnv(["ARCHIVEID=${archiveid}",
+                  "LOCALE=${locale}"]) {
+            sh("jenkins-jobs/commit-pofiles.sh")
+         }
+      }
+   }
+}
+
+
+onMaster('3h') {
+   notify([slack: [channel: '#cp-eng',
+                   sender: 'I18N Imp',
+                   emoji: ':smiling_imp:', emojiOnFailure: ':imp:',
+                   extraText: "@cp-support",
+                   when: ['FAILURE', 'UNSTABLE']],
+           email: [to: 'content-platform',
+                   when: ['FAILURE', 'UNSTABLE']],
+           aggregator: [initiative: 'content-platform',
+                        when: ['SUCCESS', 'BACK TO NORMAL',
+                               'FAILURE', 'ABORTED', 'UNSTABLE']]]) {
+      stage("Running script") {
+         runScript();
+      }
+   }
+}
+
