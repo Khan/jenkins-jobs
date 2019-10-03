0d1211819a4dd46c18e0cf582545c80c8eb8ed6b
Neha <nbagdia@gmail.com>
2019-10-03 14:53:12 -0700

    Changes to support new script being called to run locales.
    Subscribers: tom
    
    Differential Revision: https://phabricator.khanacademy.org/D58329

--- run-topictree-gen.sh
+++ run-topictree-gen.sh
@@ -5,4 +5,22 @@
 # For this script to work, secrets.py must be on the PYTHONPATH.
 
 cd webapp
-tools/devshell.py --prod --script tools/generate_topictree_json.py $1
+
+if [ ! -z "$1" ]
+then
+    flag="false"
+    echo "flag = $flag"
+    tools/devshell.py --prod --script tools/generate_topictree_json.py $1
+else
+    flag="true"
+fi
+
+if [ $flag="true" ]
+then
+    tools/devshell.py --prod --script tools/list_test_or_better_locales.py
+    filename="listfile.txt"
+    while read -r line; do
+        echo "Processing locale = $line"
+        tools/devshell.py --prod --script tools/generate_topictree_json.py $line
+    done < "$filename"
+fi
