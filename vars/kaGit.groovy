fd4ac6f0cbb7fce24ef02d7b3248ca1ac2d39937
Kai Jing <kai@khanacademy.org>
2019-11-21 14:06:57 -0800

    use sh("git") command instead of jenkins built-in git function
    Summary:
    we see a lot of timeout when using jenkins build-in
    git function to connect github.com:443 in past 2 days. '
    Now we try to use shell git command instead of
    Jenkins built-in git function.
    
    Test Plan: I test this in test-jenkins server. so far it works fine
    
    Differential Revision: https://phabricator.khanacademy.org/D59514

--- vars/kaGit.groovy
+++ vars/kaGit.groovy
@@ -27,10 +27,12 @@ def _submodulesArg(submodules) {
 def checkoutJenkinsTools() {
    // TODO(csilvers): only do this once per workspace per script.
    dir("jenkins-jobs") {
-      git(url: "https://github.com/Khan/jenkins-jobs",
-          changelog: false, poll: false);
+      // git(url: "https://github.com/Khan/jenkins-jobs",
+      //    changelog: false, poll: false);
       // The built-in 'git' step is not great: it doesn't do
       // submodules, and it messes up the tracking with the remote.
+      sh("git remote set-url origin https://github.com/Khan/jenkins-jobs.git");
+      sh("git pull");
       sh("git submodule update --init --recursive");
       sh("git branch --set-upstream-to origin/master master");
    }
