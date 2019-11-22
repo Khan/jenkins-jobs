88fb9bc59f875b08c076a3d48ff080702f2855fd
Kai Jing <kai@khanacademy.org>
2019-11-21 16:16:19 -0800

    use retry{} for git connection jenkins-jobs
    Summary:
    
    we see a lot of connection timeout to github:433
    before we figure out the root cause, we use retry {}
    to tempoary avoid timeout error.
    TODO(Kai): need to find out the root cause of timeout

--- vars/kaGit.groovy
+++ vars/kaGit.groovy
@@ -27,14 +27,18 @@ def _submodulesArg(submodules) {
 def checkoutJenkinsTools() {
    // TODO(csilvers): only do this once per workspace per script.
    dir("jenkins-jobs") {
-      // git(url: "https://github.com/Khan/jenkins-jobs",
-      //    changelog: false, poll: false);
-      // The built-in 'git' step is not great: it doesn't do
-      // submodules, and it messes up the tracking with the remote.
-      sh("git remote set-url origin https://github.com/Khan/jenkins-jobs.git");
-      sh("git pull");
-      sh("git submodule update --init --recursive");
-      sh("git branch --set-upstream-to origin/master master");
+      // we see a lot of connection timeout to github:433
+      // before we figure out the root cause, we use retry {}
+      // to tempoary avoid timeout error.
+      // TODO(Kai): need to find out the root cause of timeout
+       retry {
+         git(url: "https://github.com/Khan/jenkins-jobs",
+            changelog: false, poll: false);
+         // The built-in 'git' step is not great: it doesn't do
+         // submodules, and it messes up the tracking with the remote.
+         sh("git submodule update --init --recursive");
+         sh("git branch --set-upstream-to origin/master master");
+       }
    }
 }
 
