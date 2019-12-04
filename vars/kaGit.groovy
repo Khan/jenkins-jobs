cd6a67d7df9e35a844ef545a64bd4d68cb4d03a6
Kai Jing <kai@khanacademy.org>
2019-12-03 16:01:58 -0800

    move tcpdump out off retry

--- vars/kaGit.groovy
+++ vars/kaGit.groovy
@@ -31,10 +31,10 @@ def checkoutJenkinsTools() {
       //    changelog: false, poll: false);
       // The built-in 'git' step is not great: it doesn't do
       // submodules, and it messes up the tracking with the remote.
+      sh("sudo tcpdump -i any  -w /tmp/tcpdump.pcap &");
       retry {
          sh("curl -s -HMetadata-Flavor:Google http://metadata.google.internal/computeMetadata/v1/instance/hostname | cut -d. -f1; ");
          sh("git remote set-url origin https://github.com/Khan/jenkins-jobs.git");
-         sh("sudo tcpdump -i any  -w /tmp/tcpdump.pcap &");
          sh("git pull");
          sh("git submodule update --init --recursive");
          sh("git branch --set-upstream-to origin/master master");
