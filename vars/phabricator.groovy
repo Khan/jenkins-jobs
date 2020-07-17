d0af34ce39558ba4f15e69a38f64233963486e49
Dhruv Kapadia <dhruv@khanacademy.org>
2020-07-17 14:08:28 -0400

    Link from phab back to jenkins
    Test plan: none

--- vars/phabricator.groovy
+++ vars/phabricator.groovy
@@ -1,19 +1,47 @@
 // Utility module for interfacing with phabricator
+
+import java.net.URLEncoder;
+
+@Field PHABRICATOR_TOKEN = null;
+
+def initializeToken() {
+   if (!PHABRICATOR_TOKEN) {
+      // TODO(dhruv): rename this secret since it's used for more than just
+      // page-weight now
+      PHABRICATOR_TOKEN = readFile(
+        "${env.HOME}/page-weight-phabricator-conduit-token.secret").trim();
+   }
+}
+
 def submitHarbormasterMsg(buildPhid, type) {
    // type can be "pass", "fail", or "work"
    // See https://phabricator.khanacademy.org/conduit/method/harbormaster.sendmessage/
    if (buildPhid == "") {
       return
    }
+   initializeToken()
 
-   // TODO(dhruv): rename this secret since it's used for more than just
-   // page-weight now
-   def conduitToken = readFile(
-      "${env.HOME}/page-weight-phabricator-conduit-token.secret").trim();
-
-   def body = "api.token=${conduitToken}&buildTargetPHID=${buildPhid}&type=${type}"
+   def body = "api.token=${PHABRICATOR_TOKEN}&buildTargetPHID=${buildPhid}&type=${type}"
 
    def response = httpRequest httpMode: 'POST', contentType: "APPLICATION_FORM", requestBody: body, url: "https://phabricator.khanacademy.org/api/harbormaster.sendmessage"
+
+   assert response.status == 200;
+}
+
+def linkHarbormasterToJenkins(buildPhid, buildUrl) {
+   if (buildPhid == "") {
+      return
+   }
+
+   def message = "View on Jenkins"
+
+   def body = "api.token=${PHABRICATOR_TOKEN}&buildTargetPHID=${buildPhid}"+
+              "&artifactKey=jenkins-webapp-test-results&artifactType=uri" +
+              "&artifactData[name]=${java.net.URLEncoder.encode(message)}" +
+              "&artifactData[ui.external]=1" +
+              "&artifactData[uri]=${java.net.URLEncoder.encode(buildUrl)}"
+
+   def response = httpRequest httpMode: 'POST', contentType: "APPLICATION_FORM", requestBody: body, url: "https://phabricator.khanacademy.org/api/harbormaster.createartifact"
 
    assert response.status == 200;
 }
