ab4087accacc6f7988c57fe7c08c10e7d980de96
Craig Silverstein <csilvers@khanacademy.org>
2020-10-08 13:02:24 -0700

    Separate the confirmation to its own stage.
    Summary:
    It's useful for analysis purposes, to separate out how long we were
    waiting for e2e tests, vs how long we were waiting for manual
    confirmation.
    
    Also fixed up the 2ndsmoketest name to be more accurate.
    
    Issue: none
    
    Test Plan: Fingers crossed
    
    Reviewers: #devops, benkraft
    
    Reviewed By: #devops, benkraft
    
    Differential Revision: https://phabricator.khanacademy.org/D66620

--- jobs/2ndsmoketest-priming.groovy
+++ jobs/2ndsmoketest-priming.groovy
@@ -107,7 +107,7 @@ def prime() {
 
 def primeWorkers() {
    def jobs = [
-       "determine-splits": { prime() },
+       "sync-webapp": { prime() },
    ];
    for (def i = 0; i < NUM_WORKER_MACHINES; i++) {
       // A restriction in `parallel`: need to redefine the index var here.
