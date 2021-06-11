8329bb12fd4476e1e7b473d120fd43bb1e4a26c9
Craig Silverstein <csilvers@khanacademy.org>
2021-06-11 08:37:32 -0700

    Add a jenkins job to build the publish-worker.
    This is going to become more important as we start to need secrets in
    the publish binary, since jenkins has access to secrets on keeper.
    
    Issue: https://khanacademy.slack.com/archives/C01TYEZQA2E/p1623422572161100
    
    Auditors: benkraft, aasmund
    
    Test plan:
    Fingers crossed -- will try running this once it's landed!

---
+++ jobs/build-publish-image.groovy
@@ -0,0 +1,52 @@
+// Pipeline job that creates a new dockerfile image for the publish-worker,
+// and uploads it to our docker container-registry.
+
+@Library("kautils")
+// Classes we use, under jenkins-jobs/src/.
+import org.khanacademy.Setup;
+// Vars we use, under jenkins-jobs/vars/.  This is just for documentation.
+//import vars.kaGit
+//import vars.notify
+//import vars.onWorker
+
+
+new Setup(steps
+
+).addStringParam(
+    "GIT_REVISION",
+    """The name of a webapp branch to use when building the docker image.
+Most of the time master (the default) is the correct choice. But it can
+be any commit that is "later" than the one for the last publish worker.""",
+    "master"
+
+).addStringParam(
+    "ZND_NAME",
+    """If you specified the commit of a znd in GIT_REVISION, put the
+znd's name here.  It will be appended to the docker image name, to
+indicate that this image does not derive from prod.""",
+    ""
+
+).apply();
+
+
+def runScript() {
+   kaGit.safeSyncToOrigin("git@github.com:Khan/webapp",
+                          params.GIT_REVISION);
+
+   dir("webapp") {
+       sh("content_editing/tools/publish/validate_commit_for_publish.sh");
+       exec(["env", "ZND_NAME=${params.ZND_NAME}", "content_editing/tools/publish/build_publish_image.sh"]);
+       // TODO(csilvers): report the publish-content version to slack?
+       echo("For how to use this image, see");
+       echo ("https://khanacademy.atlassian.net/wiki/spaces/CP/pages/299204611/Publish+Process+Technical+Documentation");
+   }
+}
+
+onWorker("build-worker", "30m") {
+   notify([slack: [channel: '#cp-eng',
+                   when: ['SUCCESS', 'FAILURE', 'ABORTED', 'UNSTABLE']]]) {
+      stage("Running script") {
+         runScript();
+      }
+   }
+}
