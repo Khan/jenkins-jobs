2d9f6a401ed640cf0077c1717d1b2745dfc8f770
Jeremy Gervais <jeremygervais@khanacademy.org>
2022-01-07 11:40:31 -0500

    build current sqlite with go services
    Summary:
    run_sync is being broken apart into several smaller scripts,
    many of which are in golang. This is an update to the build_current_sqlite
    script to make sure of these smaller scripts.
    
    https://khanacademy.atlassian.net/browse/INFRA-7279
    
    Test Plan: run it and hope
    
    Reviewers: csilvers
    
    Subscribers:

--- build_current_sqlite.sh
+++ build_current_sqlite.sh
@@ -1,22 +1,25 @@
 #!/bin/sh -ex
 
-# A script to build an up-to-date current.sqlite by using the
-# run_sync.sh tool.  We download the current datastore snapshots from
-# gcs, run the tool to create a current.sqlite, and upload that to
-# gcs.
+# A script to build an up-to-date current.sqlite by using various tools to seed
+# our dev environment state.  
+
+# Specifically those tools are:
+# services/users/cmd/make_dev_users - go script to create dev user accounts
+# services/admin/cmd/make_admin - go script to make 1 user a admin
+# dev/dev_appserver/sync_snapshot.py - python script to copy content snapshots from prod to the dev environment
+# dev/dev_appserver/create_user_interactions.py - python script to create user comments and whatnot to simulate some use
+# services/progress/cmd/watch-videos - go script to create some video watching data
 #
 # This script should be run from workspace-root.
 #
 # For this script to work, secrets.py must be on the PYTHONPATH.
-
 
 # Space-separated list of GCS buckets to download snapshots from.
 : ${SNAPSHOT_NAMES:="snapshot_en snapshot_es"}
 # GCS bucket to upload current.sqlite to.
 : ${CURRENT_SQLITE_BUCKET:="gs://ka_dev_sync/"}
 
-
-# fetch sync snapshots
+# Fetch sync snapshots from google storage
 for snapshot_bucket in $SNAPSHOT_NAMES; do
     gsutil cp "gs://ka_dev_sync/$snapshot_bucket" "./$snapshot_bucket"
 done
@@ -26,67 +29,86 @@ rm -f current.sqlite
 # And clean up existing memcache prefill files
 rm -rf webapp/genfiles/content_prefill
 
-# make deps
+# make sure our dependencies are installed and up to date
 cd webapp
 make deps
 
-is_first_run=1
-for snapshot_bucket in $SNAPSHOT_NAMES; do
-    # start dev server serving locally on port 9085
-    # write logs to genfiles/appserver.log
-    timeout 10h python third_party/frankenserver/dev_appserver.py \
-        --application=khan-academy \
-        --datastore_path=../current.sqlite \
-        --port=9085 \
-        --require_indexes=yes \
-        --skip_sdk_update_check=yes \
-        --automatic_restart=no \
-        --admin_port=0 \
-        --max_module_instances=1 \
-        --log_level=info \
-        --host=127.0.0.1 \
-        $DEV_APPSERVER_ARGS \
-        ./app.yaml > genfiles/appserver.log 2>&1 &
-    appserver_pid=$!
+# start dev server serving locally on port 9085
+# write logs to genfiles/appserver.log
+timeout 10h python third_party/frankenserver/dev_appserver.py \
+    --application=khan-academy \
+    --datastore_path=../current.sqlite \
+    --port=9085 \
+    --require_indexes=yes \
+    --skip_sdk_update_check=yes \
+    --automatic_restart=no \
+    --admin_port=0 \
+    --max_module_instances=1 \
+    --log_level=info \
+    --host=127.0.0.1 \
+    $DEV_APPSERVER_ARGS \
+    ./app.yaml > genfiles/appserver.log 2>&1 &
+appserver_pid=$!
 
-    trap 'tail -n100 genfiles/appserver.log' 0 HUP INT QUIT
+# if we close this script early, dump the last part of the appserver logs to the console
+trap 'tail -n100 genfiles/appserver.log' 0 HUP INT QUIT
 
-    # wait for server to start
-    sleep 60
+# We also need to start the go services
+go_pids=
+grep -l 'runtime: go' services/*/*.yaml | xargs -n1 dirname | sort -u | while read d; do
+   make -C "$d" serve &
+   $go_pids = "$go_pids $!"
+done
+# wait for everything to fully start
+sleep 60
 
-    # do sync
+# create all the dev users and make test admin an admin user
+go run ./services/users/cmd/create_dev_users/
+go run ./services/admin/cmd/make_admin --username=testadmin
+
+for snapshot_bucket in $SNAPSHOT_NAMES; do  
+    # do content sync
     locale_name=`echo "$snapshot_bucket" | awk -F"_" '{print $NF}'`
-    tools/devshell.py --host localhost:9085 \
-        --script dev/dev_appserver/run_sync.py \
-        ${is_first_run:+"--add-users"} \
-        "../$snapshot_bucket" "$locale_name"
+    tools/devshell.py --host localhost:9085  --script dev/dev_appserver/sync_snapshot.py "../$snapshot_bucket" "$locale_name"
     sleep 10
+done
 
-    # stop dev server
+# create interaction data for the users, and have testcoach watch some videos
+tools/devshell.py --host localhost:9085  --script dev/dev_appserver/create_user_interactions.py
+kaid=$(go run ./services/users/cmd/get-kaid-for-username testcoach)
+go run ./services/progress/cmd/watch-videos/ $kaid
+
+trap - 0 HUP INT QUIT
+
+# wait some extra time to make sure everything has actually shut down and
+# fully written current.sqlite to disk
+sleep 30
+
+# stop the go services. We don't do try too hard because these generally shut down cleanly.
+kill $go_pids
+
+# try to stop dev server
+kill -15 "$appserver_pid"
+
+sleep 10
+
+# If it's still here, let it know it really needs to stop
+if ps -p "$appserver_pid" > /dev/null; then
     kill -15 "$appserver_pid"
     sleep 10
-    if ps -p "$appserver_pid" > /dev/null; then
-        kill -15 "$appserver_pid"
-        sleep 10
-    fi
-    if ps -p "$appserver_pid" > /dev/null; then
-        kill -9 "$appserver_pid"
-        sleep 10
-    fi
+fi
 
-    # make sure dev-server's subprocesses are stopped too.
-    # 8011 is the pubsub emulator; 8081 is the nginx proxy.
-    lsof -t -iTCP:8011 -iTCP:8081 | xargs -r kill -15
+# Ok, that's enough of that. It really really needs to stop
+if ps -p "$appserver_pid" > /dev/null; then
+    kill -9 "$appserver_pid"
     sleep 10
-    lsof -t -iTCP:8011 -iTCP:8081 | xargs -r kill -9
+fi
 
-    # wait some extra time to make sure everything has actually shut down and
-    # fully written current.sqlite to disk
-    sleep 30
-    is_first_run=
-
-    trap - 0 HUP INT QUIT
-done
+# make sure dev-server's subprocesses are stopped too.
+# 8011 is the pubsub emulator; 8081 is the nginx proxy.
+lsof -t -iTCP:8011 -iTCP:8081 | xargs -r kill -15
+sleep 10
+lsof -t -iTCP:8011 -iTCP:8081 | xargs -r kill -9
 
 cd ..
 # upload current.sqlite and new content prefill files (deleteing old ones)
