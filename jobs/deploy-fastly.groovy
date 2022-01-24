41fa2f74da5d1f5579fbfc929c788c3c61c30d5d
Craig Silverstein <csilvers@khanacademy.org>
2022-01-24 12:55:57 -0800

    Notify in the fastly slack channel how to roll back on error. (#22)
    ## Summary:
    When a fastly deploy goes wrong, it's a panic situation.  This PR
    makes it easier to recover by always emitting, to the `#fastly` slack
    channel, the sun command to run to undo the deploy.
    
    Issue: https://github.com/Khan/internal-services/pull/71
    
    ## Test plan:
    https://jenkins.khanacademy.org/job/deploy/job/deploy-fastly/1699
    
    New output at
    https://khanacademy.slack.com/archives/CCCC6HZ4K/p1643056437020100
    
    Author: csilvers
    
    Reviewers: yogieric, ioanacrant, csilvers, #infra-platform
    
    Required Reviewers:
    
    Approved By: yogieric, ioanacrant
    
    Checks:
    
    Pull Request URL: https://github.com/Khan/jenkins-jobs/pull/22

--- jobs/deploy-fastly.groovy
+++ jobs/deploy-fastly.groovy
@@ -83,6 +83,17 @@ def installDeps() {
    }
 }
 
+def _activeVersion() {
+   def cmd = [
+       "make",
+       "-s",
+       "-C", "webapp/services/fastly-khanacademy",
+        params.TARGET == "prod" ? "active-version" : "active-version-test",
+   ];
+   withTimeout('1m') {
+      return exec.outputOf(cmd)
+   }
+}
 
 // When deploying normal webapp services we merge in master to make sure
 // the latest code is running.  We should probably do that here too but
@@ -120,6 +131,21 @@ def setDefault() {
    }
 }
 
+def notifyWithVersionInfo(oldActive, newActive) {
+   def subject = "fastly-${params.TARGET} is now at version ${newActive}";
+   def body = "To roll back to the previous version, use `sun: fastly-rollback ${params.TARGET} to ${oldActive}`";
+   def cmd = [
+       "jenkins-jobs/alertlib/alert.py",
+       "--slack=#fastly",
+       "--chat-sender=Fastly Flamingo",
+       "--icon-emoji=:flamingo:",
+       "--severity=info",
+       "--summary=${subject}",
+   ];
+   withSecrets() {     // to talk to slack
+      sh("echo ${exec.shellEscape(body)} | ${exec.shellEscapeList(cmd)}");
+   }
+}
 
 onMaster('30m') {
    notify([slack: [channel: '#fastly',
@@ -135,6 +161,8 @@ onMaster('30m') {
          installDeps();
       }
 
+      def oldActive = _activeVersion();
+
       stage("Deploying") {
          if (params.TARGET == "prod") {
             ensureUpToDate();
@@ -148,5 +176,8 @@ onMaster('30m') {
       stage("Setting default") {
          setDefault();
       }
+
+      def newActive = _activeVersion();
+      notifyWithVersionInfo(oldActive, newActive);
    }
 }
