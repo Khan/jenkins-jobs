da1f9f9027fcf9ff5e7caa2e437258bd14e02961
Michael Polyak <michaelpolyak@khanacademy.org>
2023-03-27 16:24:25 -0400

    Reorder after git sync

--- jobs/build-publish-image.groovy
+++ jobs/build-publish-image.groovy
@@ -38,11 +38,11 @@ someone else's publish-deploy on purpose.""",
 currentBuild.displayName = "${currentBuild.displayName} (${params.GIT_REVISION})";
 
 def runScript() {
-   // Prune docker images before building if under 1.5GB of disk space.
-   sh("[ $(df -BM --output=avail . | tr -cd 0-9) -gt 1500 ] || docker image prune -af")
-
    kaGit.safeSyncToOrigin("git@github.com:Khan/webapp",
                           params.GIT_REVISION);
+
+   // Prune docker images before building if under 1.5GB of disk space.
+   sh("[ $(df -BM --output=avail . | tr -cd 0-9) -gt 1500 ] || docker image prune -af")
 
    dir("webapp") {
        if (params.VALIDATE_COMMIT) {
