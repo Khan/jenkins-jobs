24e91196961d2a1def3ae913eba395559cbc5a7c
Craig Silverstein <csilvers@khanacademy.org>
2023-05-11 13:34:34 -0700

    Get rid of unused jenkins jobs and scripts.
    I found the unused jobs by running this command on jenkins-server, as
    the jenkins user:
    ```
    grep -roh 'jobs/[^ ]*.groovy' ~/jobs/*/jobs/*/*.xml | sort -u
    ```
    This shows all the groovy scripts that are run by currently configured
    jobs.
    
    I then compared this against what's in github, and removed the three
    jobs that were in github but not in a config file.
    
    Then for every file in the top-level jenkins-jobs repo, I ran
    `git grep <filename>`, and deleted all the ones with no hits.
    That was most of them!
    
    Issue: none
    
    Test plan:
    Fingers crossed, we can always revert if we need to

--- busy_wait_on_dropbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -ex
-
-# Wait to make sure the dropbox client has fully synced all the files.
-
-dir="$1"
-while HOME=/mnt/dropbox dropbox.py filestatus "$dir" | grep -v "up to date"; do
-    echo "Waiting for $dir to be up to date"
-    sleep 30
-done
