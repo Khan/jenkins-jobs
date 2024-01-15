90a50bc1196df40910c46d2d609391d2b8ab7b21
Craig Silverstein <csilvers@khanacademy.org>
2024-01-14 20:44:50 -0800

    get rid of withSecrets(), which is no longer used

--- vars/withSecrets.groovy
+++ vars/withSecrets.groovy
@@ -7,43 +7,9 @@ import groovy.transform.Field
 // TODO(benkraft): In principle this should be per-directory; in practice
 // assuming we're always in the same directory is good enough at present.
 // TODO(benkraft): Make sure updates to this are actually atomic.
-@Field _activeSecretsBlocks = 0;
 @Field _activeSlackSecretsBlocks = 0;
 @Field _activeSlackAndStackdriverSecretsBlocks = 0;
 @Field _activeGithubSecretsBlocks = 0;
-
-def _secretsPasswordPath() {
-   return "${env.HOME}/secrets_py/secrets.py.aes.password";
-}
-
-// This must be called from workspace-root.
-def call(Closure body) {
-   try {
-      // First, set up secrets.
-      // This decryption command was modified from the make target
-      // "secrets_decrypt" in the webapp project.
-      // Note that we do this even if ACTIVE_SECRETS_BLOCKS > 0;
-      // secrets.py.aes might have changed.
-      exec(["openssl", "aes-256-cbc", "-d", "-md", "sha256", "-salt",
-            "-in", "webapp/shared/secrets.py.aes",
-            "-out", "webapp/shared/secrets.py",
-            "-kfile", _secretsPasswordPath()]);
-      sh("chmod 600 webapp/shared/secrets.py");
-      _activeSecretsBlocks++;
-
-      // Then, tell alertlib where secrets live, and run the wrapped block.
-      withEnv(["ALERTLIB_SECRETS_DIR=${pwd()}/webapp/shared"]) {
-         body();
-      }
-   } finally {
-      _activeSecretsBlocks--;
-      // Finally, iff we're exiting the last withSecrets block, clean up
-      // secrets.py so if the next job intends to run without secrets, it does.
-      if (!_activeSecretsBlocks) {
-         sh("rm -f webapp/shared/secrets.py webapp/shared/secrets.pyc");
-      }
-   }
-}
 
 // This must be called from workspace-root.  While this is in scope,
 // *only* the slack secret is available, even if there's a higher-up
