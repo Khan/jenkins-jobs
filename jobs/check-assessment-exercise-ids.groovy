e3fa432471e3a9a12c5160df6cb101c94de25f64
Tim McCabe <timmccabe@khanacademy.org>
2025-01-22 18:46:27 -0500

    Add job to validate exercise IDs in assessments

---
+++ jobs/check-assessment-exercise-ids.groovy
@@ -0,0 +1,41 @@
+// Loads all the assessment configs from datastore and checks to make sure that
+// their ExerciseIDs are valid (i.e., that there are published exercises with
+// those IDs).
+
+@Library("kautils")
+// Classes we use, under jenkins-jobs/src/.
+import org.khanacademy.Setup;
+
+
+new Setup(steps).apply();
+
+
+def _setupWebapp() {
+   withTimeout('1h') {
+      kaGit.safeSyncToOrigin("git@github.com:Khan/webapp", "master");
+   }
+}
+
+
+def runScript() {
+   withTimeout('1h') {
+      dir("webapp") {
+         sh("go run services/assessments/cmd/check_assessments_exercise_ids/main.go");
+      }
+   }
+}
+
+
+onMaster('1h') {
+   notify([slack: [channel: '#assessments-alerts',
+                   sender: 'Assessment Config Anaconda',
+                   emoji: ':snake:',
+                   when: ['FAILURE', 'UNSTABLE', 'ABORTED']]]) {
+      stage("Initializing webapp") {
+         _setupWebapp();
+      }
+      stage("Notifying") {
+         runScript();
+      }
+   }
+}
