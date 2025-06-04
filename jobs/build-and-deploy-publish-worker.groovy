b8999914a6af3c1f100c6db7fc4822fd114bff22
Jack Zhang <68999131+zhiyi-zhang-duke@users.noreply.github.com>
2025-06-04 10:16:39 -0400

    Added new script to build and deploy publish worker (#323)
    ## Summary:
    This script will be used to build the publish worker image and also deploy it to google cloud whilst following the publish techinical process procedure for verifying publish content version via the `services/content-editing/cmd/update-publish-worker/main.go` script.
    
    This script also references a newly created piece of documentation:
    https://khanacademy.atlassian.net/wiki/spaces/CP/pages/3566764137/Publish+Worker+Automated+Deploy
    Which details how the new deploy script works.
    
    This script will be called by:
    https://jenkins.khanacademy.org/job/deploy/job/auto-deploy-publish/
    To kick off an image build as well as a optionally deploying the image to production google cloud containers running the publish worker.
    
    Issue: CP-9027
    
    ## Test plan:
    
    - [X] Create a new jenkins job copying the original build-publish-worker job
    - [X] Add new deploy_to_prod parameter
    - [X] Point job to new script in this pr
    - [X] Run the job with deploy_to_prod unchecked
    - [x] Run the job with deploy_to_prod checked
    - [ ] Verify that publish is disabled, empty locales are run, stdout and stderr are streamed to jenkins job
    - [ ] Verify in google cloud that docker containers are normal after job run
    - [ ] Inform team of new jenkins job
    
    Author: zhiyi-zhang-duke
    
    Reviewers: mpolyak, zhiyi-zhang-duke, csilvers
    
    Required Reviewers:
    
    Approved By: mpolyak, csilvers
    
    Checks:
    
    Pull Request URL: https://github.com/Khan/jenkins-jobs/pull/323

---
+++ jobs/build-and-deploy-publish-worker.groovy
@@ -0,0 +1,107 @@
+// Pipeline job that creates a new dockerfile image for the publish-worker,
+// and uploads it to our docker container-registry.
+
+@Library("kautils")
+// Classes we use, under jenkins-jobs/src/.
+import org.khanacademy.Setup;
+// Vars we use, under jenkins-jobs/vars/.  This is just for documentation.
+//import vars.kaGit
+//import vars.notify
+//import vars.onWorker
+
+
+new Setup(steps
+
+).addStringParam(
+    "GIT_REVISION",
+    """The name of a webapp branch to use when building the docker image.
+Most of the time master (the default) is the correct choice. But it can
+be any commit that is "later" than the one for the last publish worker.""",
+    "master"
+
+).addStringParam(
+    "ZND_NAME",
+    """If you specified the commit of a znd in GIT_REVISION, put the
+znd's name here.  It will be appended to the docker image name, to
+indicate that this image does not derive from prod.""",
+    ""
+
+).addBooleanParam(
+    "VALIDATE_COMMIT",
+    """If set, check that this commit is safe to publish.  <b>UNSET
+WITH CAUTION!</b> -- only for situations where you are overwriting
+someone else's publish-deploy on purpose.""",
+    true
+
+).addBooleanParam(
+    "DEPLOY_TO_PROD",
+    """If set, actually deploy the new publish-worker image to production. If not set, the job will stop after building the image.""",
+    false
+// TODO(jackz): Consider using input here
+).addStringParam(
+    "DEPLOYER_EMAIL",
+    """Your @khanacademy.org email address is required if deploying the newly built publish-worker image to production.""",
+    ""
+).apply();
+
+currentBuild.displayName = "${currentBuild.displayName} (${params.GIT_REVISION})";
+
+def setupWebapp() {
+    withTimeout('1h') {
+        kaGit.safeSyncToOrigin("git@github.com:Khan/webapp", 
+            params.GIT_REVISION);
+    }
+}
+
+def runScript() {
+    // Prune docker images before building if under 1.5GB of disk space.
+    sh('[ $(df -BM --output=avail . | tr -cd 0-9) -gt 1500 ] || docker image prune -af');
+
+    dir("webapp") {
+        if (params.VALIDATE_COMMIT) {
+            sh("services/content-editing/publish/tools/validate_commit_for_publish.sh");
+        }
+        def buildOutput = exec.outputOf(["env", "ZND_NAME=${params.ZND_NAME}", "services/content-editing/publish/tools/build_publish_image.sh"]);
+        echo("For how to use this image, see");
+        echo("https://khanacademy.atlassian.net/wiki/spaces/CP/pages/299204611/Publish+Process+Technical+Documentation");
+
+        if (!params.DEPLOY_TO_PROD) {
+            echo("DEPLOY_TO_PROD is not set. Skipping deployment to production.");
+            return;
+        }
+        def matcher = (buildOutput =~ /export PUBLISH_VERSION=([\w.-]+)/);
+        def publishVersion = matcher ? matcher[0][1] : null;
+        matcher = null;
+        if (!publishVersion) {
+            notify.fail("Could not extract PUBLISH_VERSION from build_publish_image.sh output");
+        }
+
+        def deployerEmail = params.DEPLOYER_EMAIL;
+        if (!deployerEmail || !deployerEmail.endsWith("@khanacademy.org")) {
+            notify.fail("Could not determine a valid deployer email. DEPLOYER_EMAIL is missing or is a @khanacademy.org address: ${deployerEmail}");
+        }
+        def goArgs = [
+            "go", "run", "services/content-editing/cmd/update-publish-worker/main.go",
+            "--publish-image-version=${publishVersion}",
+            "--publish-image-version-including-master",
+            "--yes-really-update-publish-worker-in-production",
+            "--deployer-email=${deployerEmail}"
+        ];
+        ansiColor('xterm') {
+            exec(goArgs);
+        }
+        echo("For additional information about the publish worker deploy script, see");
+        echo("https://khanacademy.atlassian.net/wiki/spaces/CP/pages/3566764137/Publish+Worker+Automated+Deploy");
+    }
+}
+
+onWorker("build-worker", "60m") {
+    notify([slack: [channel: '#cp-eng', when: ['SUCCESS', 'FAILURE', 'ABORTED', 'UNSTABLE']]]) {
+        stage("Initializing webapp") {
+            setupWebapp();
+        }
+        stage("Running script") {
+            runScript();
+        }
+    }
+}
