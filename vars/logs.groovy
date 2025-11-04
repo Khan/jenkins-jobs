02658b7d48de0207d5f47f4587eaf7c3f9a2daa4
Jeremy Wiebe <jeremy@khanacademy.org>
2025-11-04 12:31:32 -0800

    Encode spaces as %20 instead of URLEncoder's choice of +

--- vars/logs.groovy
+++ vars/logs.groovy
@@ -1,15 +1,18 @@
 // Returns the Google Cloud Log Viewer URL for the given Version ID
 String logViewerUrl(String version_id) {
     // This query is a bit tricky. It contains newlines and double quotes
-    // intentionally and uses a "here document" (not sure what its called in
-    // Groovy, but its the 3 quotation marks that start/end a multi-line string
-    // thing)
+    // intentionally and uses a multiline string (the 3 quotation marks
+    // delimiters)
     def query = """resource.type="gae_app" OR resource.type="cloud_run_revision"
                   |resource.labels.version_id="${version_id}\"""".stripMargin();
+
+    // URLEncoder encodes spaces as "+", but Google doesn't like that so we
+    // manually replace them with a URL-encoded space (ie. "%20").
+    query = java.net.URLEncoder.encode(query);
+    query = query.replaceAll(/\+/, "%20");
 
     // Yes! We specify "query;query=..." here. That's intentional and how GCP
     // wants it.
     return "https://console.cloud.google.com/logs/query;" +
-           "query=" + java.net.URLEncoder.encode(query, "UTF-8") +
-           "?project=khan-academy";
+           "query=${query}?project=khan-academy";
 }
