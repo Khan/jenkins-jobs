f55399d033911702af19ebb9985a64df466e5e96
Jeremy Wiebe <jeremy@khanacademy.org>
2025-11-25 14:34:32 -0800

    Tweak a bit to get formatting of the resulting query nicer

--- vars/logs.groovy
+++ vars/logs.groovy
@@ -1,11 +1,10 @@
-static String _buildServiceFilter(String version_id, String service) {
-  return "(resource.labels.service_name=\"{service}\" AND " +
-         "resource.labels.revision_name=\"${service}-rev-${version_id}\")"
+String _buildServiceFilter(String version_id, String service) {
+  return "(resource.labels.service_name=\"${service}\" AND resource.labels.revision_name=\"${service}-rev-${version_id}\")"
 }
 
-// Returns the Google Cloud Log Viewer URL for the given Version ID and list of
-// services.
-static String logViewerUrl(String version_id, java.util.ArrayList services) {
+// Returns the Google Cloud Log Viewer URL for the given Version ID
+String logViewerUrl(String version_id, java.util.ArrayList services) {
+
     // This query is a bit tricky. It contains newlines and double quotes
     // intentionally and uses a multiline string (the 3 quotation marks
     // delimiters)
@@ -13,7 +12,7 @@ static String logViewerUrl(String version_id, java.util.ArrayList services) {
                   |(
                   |  ${services
                         .collect { _buildServiceFilter(version_id, it) }
-                        .join(" OR ") }
+                        .join(" OR\n  ") }
                   |)""".stripMargin();
 
     // URLEncoder encodes spaces as "+", but Google doesn't like that so we
