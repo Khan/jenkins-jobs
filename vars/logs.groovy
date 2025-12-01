ca48915335362c94131c8259b29c2d5d83bf3c94
Jeremy Wiebe <jeremy@khanacademy.org>
2025-12-01 12:11:32 -0800

    Post correct Google Cloud Logs Explorer url for Cloud Run services (#358)
    ## Summary:
    
    I _think_ we're all moved over to Cloud Run. So this PR adjusts Jenkins to post only Cloud Run logs links. They're already quite complicated (moreso than App Engine log filters) and so I'm removing support for GAE logs entirely (if you disagree, I'll work out the combo-filter that works for both). 
    
    Related: https://github.com/Khan/buildmaster2/pull/346
    
    Issue: LEMS-3744
    
    ## Test plan:
    
    I ran the modified function in an online Groovy playground (https://onecompiler.com/groovy/445rbnxyt).
    
    Author: jeremywiebe
    
    Reviewers: csilvers, MiguelCastillo
    
    Required Reviewers:
    
    Approved By: csilvers
    
    Checks:
    
    Pull Request URL: https://github.com/Khan/jenkins-jobs/pull/358

--- vars/logs.groovy
+++ vars/logs.groovy
@@ -1,10 +1,19 @@
+String _buildServiceFilter(String version_id, String service) {
+  return "(resource.labels.service_name=\"${service}\" AND resource.labels.revision_name=\"${service}-rev-${version_id}\")"
+}
+
 // Returns the Google Cloud Log Viewer URL for the given Version ID
-String logViewerUrl(String version_id) {
+String logViewerUrl(String version_id, java.util.ArrayList services) {
+
     // This query is a bit tricky. It contains newlines and double quotes
     // intentionally and uses a multiline string (the 3 quotation marks
     // delimiters)
-    def query = """resource.type="gae_app" OR resource.type="cloud_run_revision"
-                  |resource.labels.version_id="${version_id}\"""".stripMargin();
+    def query = """resource.type="cloud_run_revision"
+                  |(
+                  |  ${services
+                        .collect { _buildServiceFilter(version_id, it) }
+                        .join(" OR\n  ") }
+                  |)""".stripMargin();
 
     // URLEncoder encodes spaces as "+", but Google doesn't like that so we
     // manually replace them with a URL-encoded space (ie. "%20").
@@ -12,7 +21,7 @@ String logViewerUrl(String version_id) {
     query = query.replaceAll(/\+/, "%20");
 
     // Yes! We specify "query;query=..." here. That's intentional and how GCP
-    // wants it.
+    // wants it (they're called matrix parameters).
     return "https://console.cloud.google.com/logs/query;" +
            "query=${query}?project=khan-academy";
 }
