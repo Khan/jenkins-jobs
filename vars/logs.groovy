616837d08480fa50c502953d56c4384d30d70d46
Jeremy Wiebe <jeremy@khanacademy.org>
2025-12-01 15:45:28 -0800

    Revert Google Cloud Logs changes for Cloud Run (#359)
    ## Summary:
    
    PR #358 which updated log urls for Cloud Run broke things. Reverting for now.
    
    Issue: LEMS-3744
    
    ## Test plan:
    
    Author: jeremywiebe
    
    Reviewers: csilvers
    
    Required Reviewers:
    
    Approved By: csilvers
    
    Checks:
    
    Pull Request URL: https://github.com/Khan/jenkins-jobs/pull/359

--- vars/logs.groovy
+++ vars/logs.groovy
@@ -1,19 +1,10 @@
-String _buildServiceFilter(String version_id, String service) {
-  return "(resource.labels.service_name=\"${service}\" AND resource.labels.revision_name=\"${service}-rev-${version_id}\")"
-}
-
 // Returns the Google Cloud Log Viewer URL for the given Version ID
-String logViewerUrl(String version_id, java.util.ArrayList services) {
-
+String logViewerUrl(String version_id) {
     // This query is a bit tricky. It contains newlines and double quotes
     // intentionally and uses a multiline string (the 3 quotation marks
     // delimiters)
-    def query = """resource.type="cloud_run_revision"
-                  |(
-                  |  ${services
-                        .collect { _buildServiceFilter(version_id, it) }
-                        .join(" OR\n  ") }
-                  |)""".stripMargin();
+    def query = """resource.type="gae_app" OR resource.type="cloud_run_revision"
+                  |resource.labels.version_id="${version_id}\"""".stripMargin();
 
     // URLEncoder encodes spaces as "+", but Google doesn't like that so we
     // manually replace them with a URL-encoded space (ie. "%20").
@@ -21,7 +12,7 @@ String logViewerUrl(String version_id, java.util.ArrayList services) {
     query = query.replaceAll(/\+/, "%20");
 
     // Yes! We specify "query;query=..." here. That's intentional and how GCP
-    // wants it (they're called matrix parameters).
+    // wants it.
     return "https://console.cloud.google.com/logs/query;" +
            "query=${query}?project=khan-academy";
 }
